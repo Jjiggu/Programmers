63f339b4a80caf4a2ce1fb2549f769db5115be47
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-05-19 22:23:31 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,24 @@
+def solution(num):
+    answer = 0
+    count = 0
+    
+
+    
+    for count in range(1, 501):
+        if num == 1:
+            return 0
+            break
+            
+        elif count == 500:
+            return -1
+            break
+            
+        elif num % 2 == 0:
+            num = num / 2
+            if num == 1:
+                return count
+
+        elif num % 2 == 1:
+            num = (3 * num) + 1
+            if num == 1:
+                return count
