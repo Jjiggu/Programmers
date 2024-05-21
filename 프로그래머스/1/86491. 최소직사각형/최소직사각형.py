73774a3fbc9f86bfa51e81c0cb78e55c792d8e44
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-05-21 23:04:37 +0900

    [level 1] Title: 최소직사각형, Time: 3.81 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -7,20 +7,26 @@ def solution(sizes):
     
     for row, columns in sizes:
         if row < columns:
-            new_num = columns
-            columns = row
-            row = new_num
-            new_sizes.append([row, columns])
-        else:
-            new_sizes.append([row, columns])
+        #     new_num = columns
+        #     columns = row
+        #     row = new_num
+        #     new_sizes.append([row, columns])
+        # else:
+        #     new_sizes.append([row, columns])
+        # 아래 코드로 변경 가능
+            row, columns = columns, row
+            
+        row_max = max(row, row_max)
+        columns_max = max(columns, columns_max)
+            
             
     
-    for row, columns in new_sizes:
-        if row > row_max:
-            row_max = row
+#     for row, columns in new_sizes:
+#         if row > row_max:
+#             row_max = row
         
-        if columns > columns_max:
-            columns_max = columns
+#         if columns > columns_max:
+#             columns_max = columns
             
     
     
