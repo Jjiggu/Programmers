a7a6ece30a23e1d2f5ff103fce6902d7c4ac96c1
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-05-23 22:01:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -6,13 +6,9 @@ def solution(n, arr1, arr2):
     # 진수 변환 후 저장
     for ar1, ar2 in zip(arr1, arr2):
         arr1_binaryNum = format(ar1, 'b')
-        arr1_list.append(format(ar1, 'b').zfill(n))
-        
         arr2_binaryNum = format(ar2, 'b')
-        arr2_list.append(format(ar2, 'b').zfill(n))
         
         # 2진수끼리 AND 연산
-        
         and_result = format(int(arr1_binaryNum, 2) | int(arr2_binaryNum, 2), 'b')
         result.append(and_result.zfill(n))
     
