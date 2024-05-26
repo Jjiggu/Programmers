f1492fe7e3f9f82fc3f3b3569f594ee6bad7c4be
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-05-26 23:26:25 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,15 @@
+from string import ascii_lowercase
+def solution(s):
+    answer = True
+    alphabet_list = list(ascii_lowercase)
+    s = s.lower()
+    
+    if len(s) == 4 or len(s) == 6:
+        for i in s:
+            if i in alphabet_list:
+                return False
+                break
+    else:
+        return False
+                
+    return True
