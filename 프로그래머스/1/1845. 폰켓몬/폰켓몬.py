043fb4c6d29affbe012aca26a335614464012c93
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-05-27 22:15:25 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,17 +1,8 @@
-from itertools import combinations
 def solution(nums):
-    poketmon_nums = len(nums) // 2
-    # poket_list = list(combinations(nums, poketmon_nums))
-    result = []
-    answer = 0
+#     poketmon_nums = len(nums) // 2
     
-    if len(set(nums)) < poketmon_nums:
-        return len(set(nums))
-    else:
-#         for i in poket_list:
-#             result.append(list(set(i)))
-
-#             if len(list(set(i))) > answer:
-#                 answer = len(list(set(i)))
-            
-        return poketmon_nums
+#     if len(set(nums)) < poketmon_nums:
+#         return len(set(nums))
+#     else:   
+#         return poketmon_nums
+    return min(len(set(nums)), len(nums)//2)
