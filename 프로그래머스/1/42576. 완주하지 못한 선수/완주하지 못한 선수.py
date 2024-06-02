96989c3c9ecc0ae94517ad1d0ec428a319f05fa9
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-06-02 23:14:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.96 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,27 @@
 def solution(participant, completion):
-    result = {}
+    participant.sort()
+    completion.sort()
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
     
-    for person in participant:
-        if person in result:
-            result[person] += 1
-        else:
-            result[person] = 1
+# 효율성 테스트 통과한 코드 -> for문 사용이 너무 많음(블로그 참조한 코드)    
+#     result = {}
     
-    for people in completion:
-        if people in result:
-            result[people] -= 1
-        
-    for i in result:
-        if result[i] == 1:
-            return i
+#     for person in participant:
+#         if person in result:
+#             result[person] += 1
+#         else:
+#             result[person] = 1
+
+#     for people in completion:
+#         if people in result:
+#             result[people] -= 1
+    
+#     for i in result:
+#         if result[i] == 1:
+#             return i
     
 
 # 효율성 테스트 통과 못 한 코드 -> remove가 O(n)의 시간복잡도를 가짐
