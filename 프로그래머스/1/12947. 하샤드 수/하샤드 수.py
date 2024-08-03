3ea986439be13d3f01d4a83b42d68ec3570f02c2
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-04 00:21:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,11 +1,7 @@
 def solution(x):
-    sum_x = 0
+    hashadnum = sum([int(i) for i in str(x)])
     
-    for i in str(x):
-        sum_x += int(i)
-    
-    if x % sum_x == 0:
+    if x % hashadnum == 0:
         return True
-    
     else:
         return False
