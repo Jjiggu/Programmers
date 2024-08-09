ba877818905e4516540a1af96f03a3b8621ea938
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-09 15:48:13 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,12 +1,14 @@
 def solution(n):
     answer = 0
     three_n = ''
-
+    
+    # 3진수 변환 
     while(n > 0):
-        three_n += str(n % 3 )
+        three_n += str(n % 3)
         n = n // 3
-        
-    for index, num in enumerate(three_n):
-        answer += int(num) * pow(3, len(three_n) - index - 1)
-        
+    
+    # 10진수 변환
+    # for index, num in enumerate(three_n):
+    #     answer += int(num) * pow(3, len(three_n) - index - 1)
+    answer = int(three_n, 3)
     return answer
