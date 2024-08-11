5696911aad9c380882e98260244371e6cbafe697
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-11 14:17:30 +0900

    [level 2] Title: 타겟 넘버, Time: 346.36 ms, Memory: 50.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,11 @@
 def solution(numbers, target):
-    answer = 0
     result = [0]
     
     for i in range(len(numbers)):
         tmp = []
         for j in range(len(result)):
             tmp.append(result[j] + numbers[i])
-            tmp.append(result[j] - numbers[i])
-        
+            tmp.append(result[j] - numbers[i])        
         result = tmp
     
-    answer = result.count(target)
-    
-    return answer
+    return result.count(target)
