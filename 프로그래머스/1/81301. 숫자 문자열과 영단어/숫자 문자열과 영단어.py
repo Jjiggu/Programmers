780218f1017ef4d560751d48cf5df3a1f547a6e6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-12 16:28:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,17 @@
 def solution(s):
-    answer = 0
+    num_dict = {
+        "zero" : "0",
+        "one" : "1",
+        "two" : "2",
+        "three" : "3",
+        "four" : "4",
+        "five" : "5",
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine" : "9"
+    }
     
-    num_dict = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     
     for word, num in num_dict.items():
         if word in s:
