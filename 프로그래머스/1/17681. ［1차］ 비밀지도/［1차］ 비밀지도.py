1e72636a128584a7cc0137d507928c92b1a24d90
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-12 22:10:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,19 +1,15 @@
 def solution(n, arr1, arr2):
-    answer = []
-    arr1_list = []
-    arr2_list = []
     result = []
-    # 진수 변환 후 저장
+    binary_arr1 = []
+    binary_arr2 = []
+    and_result = []
+    
     for ar1, ar2 in zip(arr1, arr2):
-        arr1_binaryNum = format(ar1, 'b')
-        arr2_binaryNum = format(ar2, 'b')
+        binary_arr1 = format(ar1, 'b')
+        binary_arr2 = format(ar2, 'b')
         
-        # 2진수끼리 AND 연산
-        and_result = format(int(arr1_binaryNum, 2) | int(arr2_binaryNum, 2), 'b')
+        and_result = format(int(binary_arr1, 2) | int(binary_arr2, 2), 'b')
         
-        # 2개 겹친 배열에서 1이면 #, 0이면 " "
-        # result.append(and_result.zfill(n).replace("1", "#").replace("0", " "))
-        result.append(and_result.rjust(n, '0').replace("1", "#").replace("0", " "))
-        
+        result.append(and_result.rjust(n, '0').replace('1', '#').replace('0', ' '))
     
     return result
