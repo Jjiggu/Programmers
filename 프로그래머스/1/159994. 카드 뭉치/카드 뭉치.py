76cd7408acfeb13637ad605022d664589ab18fff
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-13 13:51:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,28 +1,20 @@
 def solution(cards1, cards2, goal):
-    answer = ''
+    result = "No"
+    test = []
     
-    for word in goal:
-        if len(cards1) > 0 and word == cards1[0]:
-            cards1.pop(0)
-        elif len(cards2) > 0 and word == cards2[0]:
-            cards2.pop(0)
-        else:
-            return 'No'
-    answer = 'Yes'
+    for i in goal:
+        if len(cards1) != 0: 
+            if cards1[0] == i:
+                a = cards1.pop(0)
+                test.append(a)
+                
+        if len(cards2) != 0:
+            if cards2[0] == i:
+                b = cards2.pop(0)
+                test.append(b)
+        
+    if test == goal:
+        return "Yes"
+    else:
+        return "No"
     
-    # 첫번째 시도
-    # for word in goal:
-    #     if word in cards1:
-    #         if word == cards1[0]:
-    #             cards1.pop(0)
-    #         else:
-    #             answer = 'No'
-    #             break
-    #     elif word in cards2:
-    #         if word == cards2[0]:
-    #             cards2.pop(0)
-    #         else:
-    #             answer = 'No'
-    #             break
-            
-    return 'Yes'
