7c04d68e9d231bb1ca96f66ebd97211e05f511b6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-23 15:14:09 +0900

    [level 1] Title: 모의고사, Time: 13.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,24 +2,23 @@ def solution(answers):
     first_supo = "12345"
     second_supo = "21232425"
     third_supo = "3311224455"
-    first_supo_num = 0
-    second_supo_num = 0
-    third_supo_num = 0
-    supo = {}
     
-    for index, num in enumerate(answers):
-        if int(first_supo[index % len(first_supo)]) == num:
-            first_supo_num += 1
+    correct_list = [0, 0, 0]
+    result = []
+    
+    for i in range(len(answers)):
+        if answers[i] == int(first_supo[i % len(first_supo)]):
+            correct_list[0] += 1
             
-        if int(second_supo[index % len(second_supo)]) == num:
-            second_supo_num += 1
+        if answers[i] == int(second_supo[i % len(second_supo)]):
+            correct_list[1] += 1
             
-        if int(third_supo[index % len(third_supo)]) == num:
-            third_supo_num += 1
+        if answers[i] == int(third_supo[i % len(third_supo)]):
+            correct_list[2] += 1
     
-    supo[1] = first_supo_num
-    supo[2] = second_supo_num
-    supo[3] = third_supo_num
-
+    for i in range(len(correct_list)):
+        if correct_list[i] == max(correct_list):
+            result.append(i + 1)
     
-    return [k for k,v in supo.items() if max(supo.values()) == v]
+    return result
+    
