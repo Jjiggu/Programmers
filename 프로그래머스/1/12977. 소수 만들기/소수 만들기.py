c14e2cfc34e0e5ccc2014a5b414cf4c88cb0e9ca
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-23 15:44:09 +0900

    [level 1] Title: 소수 만들기, Time: 57.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,19 +1,16 @@
 from itertools import combinations
 def solution(nums):
     answer = 0
-    cnt = 0
-    prime_list = []
     
     for i in combinations(nums, 3):
         prime_number = sum(i)
-        for i in range(1, prime_number + 1):
+        cnt = 0    
+        
+        for i in range(1, int(prime_number**0.5 + 1)):
             if prime_number % i == 0:
                 cnt += 1
-        if cnt == 2:
-            answer += 1
-        # prime_list.append(cnt)
-        cnt = 0
-            
-        
+                           
+        if cnt == 1:
+            answer += 1   
 
     return answer
