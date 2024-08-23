4948318dc95078a0a8dcddd672129717c75b99d0
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-23 16:26:19 +0900

    [level 1] Title: 과일 장수, Time: 137.12 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,16 +1,10 @@
 def solution(k, m, score):
-    score_list = score
-    num_box = len(score) // m
-    score_sum = 0
+    score.sort(reverse = True)
+    box_count = (len(score) // m) * m
+    result = 0
     
-    # score를 내림차순으로 정렬
-    score_list.sort(reverse=True)
-    
-    # m개씩 나누기 len(score) // m 해서 나온만큼만
-    for i in range(num_box):
-        box = score_list[m*i:m*(i+1)]
+    for i in range(0, box_count, m):
+        result += m * min(score[i: i + m])
         
-        score_sum += box[-1] * m
+    return result
     
-    # m개씩 나눈 후 sum에 추가
-    return score_sum
