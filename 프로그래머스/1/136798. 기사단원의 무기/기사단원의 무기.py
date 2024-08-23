a7202510d16abed2ec35283497a3cc71610a994f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-23 15:37:31 +0900

    [level 1] Title: 기사단원의 무기, Time: 2289.64 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,36 +1,26 @@
-def solution(number, limit, power):
-    answer = 0
-    
-    measure_list = []
-    measure_count = 0
-    
-#     for j in range(1, int(i**(1/2)) + 1):
-#         if (i % j == 0):
-#             measure_count += 1
-#             if ( (j**2) != i) : 
-#                 measure_count += 1
+def count_divisors(n):
+    """
+    주어진 숫자 n의 약수 개수를 반환하는 함수.
+    """
+    count = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            count += 1
+            if i != n // i:  # n이 완전 제곱수가 아닐 때
+                count += 1
+    return count
 
+
+def solution(number, limit, power):
+    divisors = []
     
-    # 1~number까지 약수 구하는 로직
-    for i in range(1, number+1): # number에 있는 숫자 차례대로 뽑기
-        for j in range(1, int(i**(1/2)) + 1):
-            if (i % j == 0):
-                measure_count += 1
-                if ( (j**2) != i) : 
-                    measure_count += 1
-        # for j in range(1, i + 1): # i 까지 돌면서 약수 확인
-        #     if i % j == 0:
-        #         measure_count += 1
-                
-        measure_list.append(measure_count)
-        measure_count = 0
+    for i in range(1, number + 1):
+        attack_power = count_divisors(i)
+        
+        if attack_power > limit:
+            divisors.append(power)
+        else:
+            divisors.append(count_divisors(i))
     
-    # mesure_list 돌면서 limit 보다 크면 power으로 변경
-    for index, el in enumerate(measure_list):
-        if el > limit:
-            measure_list[index] = power
-            
-    # measure_list 합을 answer로 저장
-    answer = sum(measure_list)
+    return sum(divisors)
     
-    return answer
