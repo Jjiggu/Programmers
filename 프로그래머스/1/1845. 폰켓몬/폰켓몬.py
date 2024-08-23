b7f5877b170d5f0cd3cbb94c368a10fa101f8ea2
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-23 14:51:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.78 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,2 @@
 def solution(nums):
-#     poketmon_nums = len(nums) // 2
-    
-#     if len(set(nums)) < poketmon_nums:
-#         return len(set(nums))
-#     else:   
-#         return poketmon_nums
-    return min(len(set(nums)), len(nums)//2)
+    return min(len(set(nums)), len(nums) // 2)
