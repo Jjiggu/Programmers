43aaa3decb450ad7b4640c0799945d9062fbbea4
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-26 21:47:38 +0900

    [level 1] Title: 덧칠하기, Time: 9.77 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,12 @@
 def solution(n, m, section):
-    answer = 0
+    result = 0
+    tmp = 0
     
-    # section[0]은 무조건 칠해야 함 -> 시작점을 section[0]으로 설정하고 count도 1로 초기화
-    start_point = section[0]
-    count = 1
-
-    for i in section:
-        # 다음 섹션은 start_point + m 보다 큰 섹션에서 시작해야 함 -> 보다 작으면 같은 곳을 중복해서 칠하는것이라 최소 횟수 x
-        if start_point + (m - 1) < i:
-            start_point = i
-            count += 1
-
-    # 배열이 모두 칠한 상태가 되면 끝
-    return count
+    for j in section:
+        if j > tmp and tmp < n:
+            result += 1
+            tmp = j + m - 1
+    
+    return result
+        
+    
