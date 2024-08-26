abe236332bf3b106209d9d0a0b76c6c01b9925af
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-26 23:48:50 +0900

    [level 1] Title: 옹알이 (2), Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,21 +1,13 @@
 def solution(babbling):
     result = 0
     possible_list = ["aya", "ye", "woo", "ma"]
-    word_double = ""
     
     for i in range(len(babbling)):
-        for possible in possible_list:
-            if possible * 2 not in babbling[i]:
-                # if word_double == possible:
-                #     word_double = ""
-                #     break
-                # else:
-                #     word_double = possible
-                babbling[i] = babbling[i].replace(possible, "*")
-    
+        for word in possible_list:
+            if word * 2 not in babbling[i]:
+                babbling[i] = babbling[i].replace(word, "*")
+                
         if all(char == "*" for char in babbling[i]):
             result += 1
-
-    
-    print(babbling)
+        
     return result
