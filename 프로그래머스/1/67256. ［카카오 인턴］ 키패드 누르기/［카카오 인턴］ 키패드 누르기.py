936c2358b5fe27ff5eaa0b68bc962a056b527c52
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-30 18:26:27 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,7 +1,4 @@
-import numpy as np
-
 def solution(numbers, hand):
-    answer = ''
     
     phone_num = {1:[0, 0],
                  2:[0, 1],
@@ -16,48 +13,45 @@ def solution(numbers, hand):
                  0:[3, 1],
                  "#":[3, 2] }
     
-    left_location = phone_num["*"]
-    right_location = phone_num["#"]
+    result = ''
+    left_hand = phone_num["*"]
+    right_hand = phone_num["#"]
+    l = [1, 4, 7]
+    r = [3, 6, 9]
     
-    # 이동 좌표 오른쪽 [0, +1], 왼쪽 [0, -1], 위 [+1, 0], 아래 [-1, 0]
-    for num in numbers:
-        if num in (1, 4, 7):
-            answer += "L"
-            left_location = phone_num[num]
-            print("왼쪽 번호에 있음 L")
+    for i in numbers:
+        if i in l:
+            left_hand = phone_num[i]
+            result += "L"
             
-        elif num in (3, 6, 9):
-            answer += "R"
-            right_location = phone_num[num]
-            print("오른쪽 번호에 있음 R")
+        elif i in r:
+            right_hand = phone_num[i]
+            result += "R"
             
-        elif num in (2, 5, 8, 0):
-            # 거리 비교
-            left_distance = sum(abs((np.array(phone_num[num]) - np.array(left_location))))
-            right_distance = sum(abs((np.array(phone_num[num]) - np.array(right_location))))
-            print(f"왼쪽 거리 : {left_distance}")
-            print(f"오른쪽 거리 : {right_distance}")
-            # 거리가 짧은 손가락으로 위치 초기화
-            if left_distance < right_distance:
-                left_location = phone_num[num]
-                answer += "L"
-                print("L")
+        else:
+            now_left = phone_num[i]
+            now_right = phone_num[i]
+            
+            left_dirt = abs(int(left_hand[0]) - int(now_left[0])) + abs(int(left_hand[1]) - int(now_left[1]))
+            right_dirt = abs(int(right_hand[0]) - int(now_right[0])) + abs(int(right_hand[1]) - int(now_right[1]))
+            
+            if left_dirt < right_dirt:
+                left_hand = phone_num[i]
+                result += "L"
                 
-            elif left_distance > right_distance:
-                right_location = phone_num[num]
-                answer += "R"
-                print("R")
-            # 거리가 같은 경우
-            elif left_distance == right_distance: 
-                if hand == "right":
-                    answer += "R"
-                    right_location = phone_num[num]
-                    print("같은 경우 R")
-                elif hand == "left":
-                    answer += "L"
-                    left_location = phone_num[num]
-                    print("같은 경우 L")
-                
+            if right_dirt < left_dirt:
+                right_hand = phone_num[i]
+                result += "R"
             
+            if left_dirt == right_dirt:
+                if hand == "left":
+                    left_hand = phone_num[i]
+                    result += "L"
+                else:
+                    right_hand = phone_num[i]
+                    result += "R"
     
-    return answer
+    return result
+    
+    
+    
