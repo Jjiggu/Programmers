0a3e73f224153ef44a1f00b3026a84c12d067b39
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-30 17:28:17 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,28 +1,16 @@
 def solution(board, h, w):
-    answer = 0
-    count = 0
-    n = len(board)
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    dh = [-1, 1, 0, 0] # 높이 변화(위, 아래, 왼쪽 오른쪽)
-    dw = [0, 0, -1, 1] # 높이 변화(위, 아래, 왼쪽 오른쪽)
+    result = 0
+    color = board[h][w]
     
-    ## 이런 식으로 설정하는게 더 좋은 방식인가?
-    directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-    
-    ## 좌표 설정
     for i in range(4):
-        h_check = h + dh[i]
-        w_check = w + dw[i]
+        nx = h + dx[i]
+        ny = w + dy[i]
         
-        ## 색이 같은지 확인
-        if (n > h_check >= 0) and (n > w_check >= 0):
-                # if board[h][w] == board[h_check][w_check]:
-                #     count += 1
-                # if 사용 안하고 아래 코드처럼 처리 가능
-                count += int(board[h][w] == board[h_check][w_check])
+        if 0 <= nx < len(board) and 0 <= ny < len(board):
+            if board[nx][ny] == color:
+                result += 1
                 
-    
-    # 답 저장
-    answer = count
-    
-    return answer
+    return result
