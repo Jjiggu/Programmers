24c6a0405b8ad194709bae89df83e501aa35c106
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-30 17:14:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.51 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,38 +1,24 @@
 def solution(board, moves):
-    cnt = 0
-    pop_list = []
-    same_number = -1
-    rows = len(board)
-    new_list = [[] for i in range(rows)] # 공백 리스트
+    board_list = [[] for i in range(len(board))]
+    stack = []
+    result = 0
     
-    # 각 줄별로 원소 재정렬 
-    # 0이면 비어있는거임
-    for index, ele in enumerate(board[::-1]):
-        for j in range(len(board)):
-            if ele[j] != 0:
-                new_list[j].append(ele[j])
-
-    # 반복문 돌면서 board[moves 원소 - 1][제일 위 -> :: -1] -> pop() or remove해서 새 stack에 넣기
-    for index, ele in enumerate(moves):
-        # stack에 같은 숫자 2개 되면 pop() 해서 원소 날린 후 cnt 증가
-        if len(new_list[ele - 1]) == 0:
-            continue
-        else:
-            same_number = new_list[ele - 1].pop()
+    for i in board[::-1]:
+        for j in range(len(i)):
+            if i[j] != 0:
+                board_list[j].append(i[j])
+            
+    for index in moves:
+        if len(board_list[index - 1]) != 0:
+            doll = board_list[index - 1].pop()
         
-        if len(pop_list) == 0:
-            pop_list.append(same_number)
-            # print(f"공백 리스트에 숫자 추가 : {pop_list}")
-        else:
-            if same_number == pop_list[-1]:
-                pop_list.append(same_number)
-                # print(f"숫자 같은 경우 : {pop_list}")
-                pop_list.pop()
-                pop_list.pop()
-                cnt += 2
-            else: 
-                pop_list.append(same_number)
-                # print(f"숫자 다른 경우 : {pop_list}")
+            if len(stack) != 0 and stack[-1] == doll:
+                stack.pop()
+                result += 2
+            else:
+                stack.append(doll)
     
-    return cnt
-    # 테케 통과 못하는 이유 -> 숫자가 0인 경우 비어있기 때문에 0이 아닌 수부터 pop() 해야 됨
+    print(stack)
+    
+    return result
+        
