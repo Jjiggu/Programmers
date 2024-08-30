8dfb437d208bcbd80070ba7c10279e8c9d288007
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-08-30 16:46:22 +0900

    [level 1] Title: 햄버거 만들기, Time: 242.90 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -4,7 +4,7 @@ def solution(ingredient):
     result = 0
     for i in ingredient:
         stack.append(i)
-        if len(stack) >= 4 and stack[len(stack) - 4:] == [1, 2, 3, 1]:
+        if stack[-4:] == [1, 2, 3, 1]:
             result += 1
             for _ in range(4):
                 stack.pop()
