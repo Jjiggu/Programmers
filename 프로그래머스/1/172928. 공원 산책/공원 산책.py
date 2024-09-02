14541949dc74766dac04bc2c916969be95202740
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-09-03 00:53:27 +0900

    [level 1] Title: 공원 산책, Time: 0.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,40 +1,40 @@
-def find_start_location(park):
-    for y_index, y in enumerate(park):
-        if "S" in y:
-            return[y_index, y.index("S")]
-    return [-1, -1]
-
-def move_location(dog_location, direction, distance, park):
-    initial_y, initial_x = dog_location
-    y, x = dog_location
-    
-    for i in range(distance):
-        one_mov = [a+b for a, b in zip(dog_location, direction)]
-        y, x = one_mov
-        
-        try:
-            if(park[y][x] != "X" and (0 <= y < len(park)) and (0 <= x < len(park[0]))):
-                dog_location = [y, x]
-            else:
-                return [initial_y, initial_x]
-        except IndexError:
-            return [initial_y, initial_x]
-        
-    return [y, x]
-
+def find_start(park):
+    for i in range(len(park)):
+        for j in range(len(park[i])):
+            if park[i][j] == "S":
+                return [i, j]
+                
+            
 def solution(park, routes):
     answer = []
-    
-    dog_location = find_start_location(park)
-    
     direction = {"N":[-1, 0], "S":[1, 0], "W":[0, -1], "E":[0, 1]}
     
-    for route in routes:
-        
-        mov_dir = direction[route[0]]
-        mov_dis = int(route[2])
-        
-        dog_location = move_location(dog_location, mov_dir, mov_dis, park)
+    dog_location = find_start(park)
     
-    
+    for i in routes:
+        op, n = i.split(" ")
+        intitial_y, intitial_x = dog_location
+        y, x = dog_location
+        
+        for j in range(int(n)):
+            now_direction = direction[op]
+            
+            y, x = [y + now_direction[0], x + now_direction[1]]
+            
+            # one_move = [a + b for a, b in zip(dog_location, now_direction)]
+            # y, x = one_move
+            
+            try:
+                if park[y][x] != "X" and (0 <= y < len(park)) and (0 <= x < len(park[0])):
+                    dog_location = [y, x]
+                else:
+                    dog_location = [intitial_y, intitial_x]
+                    break
+                    
+            except IndexError:
+                dog_location = [intitial_y, intitial_x]
+                break
+        
+        
     return dog_location
+            
