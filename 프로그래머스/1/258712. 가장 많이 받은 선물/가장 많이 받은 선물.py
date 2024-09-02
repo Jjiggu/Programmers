8294ac95e3dc208d3619871ecf55e62073aeb033
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-09-03 02:20:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 11.56 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,66 +1,39 @@
 def solution(friends, gifts):
-    gift_list = {}
-    giver_list = {}
-    receiver_list = {}
-    answer_list = {}
+    give = {i:[] for i in friends}
+    receive = {i:[] for i in friends}
+    gift_pont = {i:0 for i in friends}
     
-    # friends 등록
-    for name1 in friends:
-        inner_gift_map = {}
-        for name2 in friends:
-            inner_gift_map[name2] = 0
-        gift_list[name1] = inner_gift_map
-        answer_list[name1] = 0
-        giver_list[name1] = 0
-        receiver_list[name1] = 0
+    # gift_list = [[0] * len(friends)] * len(friends)
+    gift_list = [[0] * len(friends) for i in range(len(friends))]
+    
+    friend_num = {}
+    
+    result = [0] * len(friends)
+    
+    for idx, friend in enumerate(friends):
+        friend_num[friend] = idx
+    
+    for i in range(len(gifts)):
+        giver, reciever = gifts[i].split(" ")
         
-    # 선물 관련 데이터 저장
-    for gift in gifts:
-        giver, receiver = gift.split()
-        gift_list[giver][receiver] += 1
-        giver_list[giver] += 1
-        receiver_list[receiver] += 1
+        gift_list[friend_num[giver]][friend_num[reciever]] += 1
         
-    # 선물 비교
-    for i in range(len(friends) - 1):
-        for j in range(i + 1, len(friends)):
-            A = friends[i]
-            B = friends[j]
+        give[giver].append(reciever)
+        receive[reciever].append(giver)
+    
+    
+    for g, r, name in zip(give.values(), receive.values(), friends):
+        gift_pont[name] = len(g) - len(r)
+        
+    
+    for i in range(len(friends)):
+        for j in range(len(friends)):
+            if gift_list[i][j] > gift_list[j][i]:
+                result[i] += 1
             
-            A_count = gift_list[A][B]
-            B_count = gift_list[B][A]
-            
-            if A_count > B_count:
-                answer_list[A] += 1
-            elif B_count > A_count:
-                answer_list[B] += 1
-            else:
-                A_value = giver_list[A] - receiver_list[A]
-                B_value = giver_list[B] - receiver_list[B]
-                
-                if A_value > B_value:
-                    answer_list[A] += 1
-                elif A_value < B_value:
-                    answer_list[B] += 1
+            elif gift_list[i][j] == gift_list[j][i]:
+                if gift_pont[friends[i]] > gift_pont[friends[j]]:
+                    result[i] += 1
     
+    return max(result)
     
-    
-#     # 초기 배열 선언
-#     for i in range(len(friends)):
-#         for j in range(len(friends)):
-#             gift_list[i][j].append(friends[i])
-#             gift_list[i][j].append(friend[j])
-    
-#     # gifts 와 값 비교하여 선물 회수 초기화
-#     for name in gifts:
-#         for i in range(len(friends)):
-#             for j in range(len(friends)):
-#                 if gift_list[i][j] == name.strip():
-#                     dg_list[i][j].append(int(1)
-    
-#     # 선물지수
-#     for i in range(len(friends)):
-#         gift_score.append(sum(dg_list[i][])-sum(dg_list[j][i]))
-    
-#     answer = 0
-    return max(answer_list.values())
