320403ece6a310ed366c343e4846952dd3a80a01
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-09-07 23:38:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,39 +1,39 @@
 def solution(bandage, health, attacks):
-    answer = 0
-    
-#     # 공격
-#     for attack in range()
-    
-#     # 스킬 사용
-#     for i in range(bandage[0]):
-#         if now_hp <= health: # 현재 체력이 최대 체력보다 작은 경우 회복 
-#             health += bandage[1]
-            
-#     if (now_hp + bandage[2]) <= health: # 스킬 사용이 끝난 후 추가 회복량과 최대체력 비교
-#         now_hp += bandage[2]  # 최대 체력을 넘지 않으면 추가 체력 회복
-#     else:
-#         now_hp = health # 최대 체력까지만 회복
-
-    cast_time, healing_per_second, additional_healing = bandage
-    
+    active_t, heal_second, extra_heal = bandage
+    now_health = health
     max_health = health
-    per_attack_time = attacks[0][0]
+    now_time = 0
+    skill_time = 1
     
-    for attack_time, damage in attacks:
-        time_between = attack_time - per_attack_time -1
-    
-        if time_between > 0:
-            health += (time_between * healing_per_second)
-            health += ((time_between // cast_time) * additional_healing)
-
-            if health > max_health:
-                health = max_health
-        health -= damage
-
-        if health <= 0:
-            return -1
-
-        per_attack_time = attack_time
-    
+    for attack in attacks:
+        attack_t, attack_d = attack
+            
+        for time in range(now_time, attack_t):
+            if skill_time == active_t:
+                if now_health + extra_heal + heal_second > max_health:
+                    now_health = max_health
+                    skill_time = 1
+                    continue
+                else:
+                    now_health += (extra_heal + heal_second)
+                    skill_time = 1
+                    continue
+            
+            if now_health + heal_second > max_health:
+                skill_time += 1
+                now_health = max_health
+                
+            elif now_health + heal_second <= max_health:
+                skill_time += 1
+                now_health += heal_second
+            
+        now_health -= attack_d
         
-    return health
+        if now_health <= 0:
+            return -1
+        
+        skill_time = 1
+        now_time = attack_t + 1
+        
+        
+    return now_health
