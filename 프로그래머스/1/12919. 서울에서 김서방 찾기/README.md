f611dd1be55027b078e4cbffddc35060fa37caf4
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-10-25 23:08:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.24 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
 
 ### 성능 요약
 
-메모리: 81.4 MB, 시간: 2.10 ms
+메모리: 76.8 MB, 시간: 1.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 19:39:43
+2024년 10월 25일 23:07:59
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String[] seoul) {
-        int kim_index = Arrays.asList(seoul).indexOf("Kim");
-        String result = String.format("김서방은 %d에 있다", kim_index);
+        int kimIndex = Arrays.asList(seoul).indexOf("Kim");
+        String result = String.format("김서방은 %d에 있다", kimIndex);
         
         return result;
     }
