eb54277d960fc368dba536a515ed424ec481247d
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-10-29 20:58:11 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,18 +1,11 @@
 def solution(n, lost, reserve):
-    # 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복 빌려줄 수 있음 -> 최대한 많은 학생이 체육수업을 들어야 함
-    # 학생의 수 n, 체육복을 도난당한 학생들의 번호 lost, 여벌의 체육복을 가져온 학생 reserve
-    # 여별 체육복이 있는 학생이 도난 가능
-    result = 0
-    # 정렬
+    result = 0    
     lost.sort()
     reserve.sort()
     
-    # 체육복이 없는 학생 구하기
     non_reserve = [i for i in lost if i not in reserve]
-    # 여별 체육복 있는 학생 
     reserve = [i for i in reserve if i not in lost]
-    
-    # 배열 돌면서(여벌 있는데 도난 당한 학생 제외) lost에게 빌려주기 
+            
     for i in range(1, n + 1):
         if i not in non_reserve and i not in reserve:
             result += 1
@@ -31,5 +24,7 @@ def solution(n, lost, reserve):
             result += 1
     
     
-    return result
+    # return result
+    return n - len(non_reserve)
+    
     
