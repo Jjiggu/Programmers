c22996b9e9a92e16cb072e021d8258d6d3030ab5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-12-18 22:19:28 +0900

    [Gold II] Title: 불켜기, Time: 240 ms, Memory: 25856 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.java
+++ 백준/Gold/11967. 불켜기/불켜기.java
@@ -6,9 +6,7 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
-    static int result;
     static int N, M;
-    static int x, y, a, b;
     static boolean[][] lights;
     static boolean[][] visited;
     static ArrayList<Room>[][] room;
@@ -16,7 +14,6 @@ public class Main {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static Queue<Room> q;
-
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,59 +28,55 @@ public class Main {
 
         q = new LinkedList<>();
 
-
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; j < N + 1; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 room[i][j] = new ArrayList<>();
             }
         }
 
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
             room[x][y].add(new Room(a, b));
         }
 
-        result = 1;
-        q = new LinkedList<>();
-
-        q.add(new Room(1, 1));
-        visited[1][1] = true;
         lights[1][1] = true;
+        visited[1][1] = true;
+        q.add(new Room(1, 1));
+        int result = 1;
 
-        bfs();
-
-        System.out.println(result);
-    }
-
-
-    public static void bfs() {
         while (!q.isEmpty()) {
-            Room tmp = q.poll();
+            Room current = q.poll();
 
-            if (!room[tmp.x][tmp.y].isEmpty()) {
-                visited = new boolean[N + 1][N + 1];
-                visited[tmp.x][tmp.y] = true;
+            // 현재 방의 스위치 작동
+            for (Room r : room[current.x][current.y]) {
+                if (!lights[r.x][r.y]) {
+                    lights[r.x][r.y] = true;
+                    result++;
 
-                for (Room room1 : room[tmp.x][tmp.y]) {
-                    if (!lights[room1.x][room1.y]) {
-                        lights[room1.x][room1.y] = true;
-                        result++;
+                    // 새로 불이 켜진 방이 인접한 방문된 방이 있는지 확인
+                    for (int i = 0; i < 4; i++) {
+                        int nx = r.x + dx[i];
+                        int ny = r.y + dy[i];
+                        if (nx > 0 && ny > 0 && nx <= N && ny <= N) {
+                            if (visited[nx][ny]) {
+                                q.add(new Room(r.x, r.y));
+                                visited[r.x][r.y] = true;
+                                break;
+                            }
+                        }
                     }
                 }
-                room[tmp.x][tmp.y].clear();
             }
 
-
+            // 인접한 방으로 이동
             for (int i = 0; i < 4; i++) {
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
-
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
 
                 if (nx > 0 && ny > 0 && nx <= N && ny <= N) {
                     if (lights[nx][ny] && !visited[nx][ny]) {
@@ -93,6 +86,8 @@ public class Main {
                 }
             }
         }
+
+        System.out.println(result);
     }
 }
 
@@ -100,9 +95,8 @@ class Room {
     int x;
     int y;
 
-    Room (int x, int y) {
+    Room(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
