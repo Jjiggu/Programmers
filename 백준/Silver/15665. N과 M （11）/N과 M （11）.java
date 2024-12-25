b02c8828ae978f6697a0b3073366598a5160c0a1
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2024-12-25 14:07:02 +0900

    [Silver II] Title: N과 M (11), Time: 348 ms, Memory: 33804 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static int[] arr;
+    static boolean[] isUsed;
+    static int MAX = 8;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        nums = new int[N];
+        arr = new int[MAX];
+        isUsed = new boolean[MAX];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        back(0);
+
+        System.out.print(sb.toString());
+    }
+
+
+    public static void back(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prevNum = -1;
+
+        for (int i = 0; i < N; i++) {
+            if (nums[i] != prevNum) {
+                arr[k] = nums[i];
+                prevNum = nums[i];
+                back(k + 1);
+            }
+        }
+    }
+}
