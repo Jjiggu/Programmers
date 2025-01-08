60085bf1f598737e14566c186b9a2c9ce0ef8e88
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-08 13:27:32 +0900

    [Gold V] Title: 애너그램, Time: 276 ms, Memory: 28000 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static char[] word;
+    static int[] word;
     static char[] arr;
-    static boolean[] isUsed;
     static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
@@ -18,53 +14,39 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
 
-        Set<String> uniquePermutations = new HashSet<>();
-
         for (int i = 0; i < N; i++) {
             sb = new StringBuilder();
+            word = new int[26];
 
             String line = br.readLine();
 
-            word = new char[line.length()];
             arr = new char[line.length()];
-            isUsed = new boolean[line.length()];
 
             for (int j = 0; j < line.length(); j++) {
-                word[j] = line.charAt(j);
+                word[line.charAt(j) - 'a']++;
             }
 
-            Arrays.sort(word);
+            back(0, line.length());
 
-            back(0, uniquePermutations);
-
-            uniquePermutations.stream().sorted().forEach(s -> sb.append(s).append("\n"));
-            System.out.print(sb);
-
-            uniquePermutations.clear();
+            System.out.print(sb.toString());
         }
     }
 
-    public static void back(int k, Set<String> uniquePermutations) {
-        if (k == word.length) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < word.length; i++) {
-                sb.append(arr[i]);
+    public static void back(int k, int maxSize) {
+        if (k == maxSize) {
+            for (int j = 0; j < maxSize; j++) {
+                sb.append(arr[j]);
             }
-            uniquePermutations.add(sb.toString());
+            sb.append("\n");
             return;
         }
 
-        for (int j = 0; j < word.length; j++) {
-            if (!isUsed[j]) {
-                // 중복된 문자를 건너뛰도록 개선
-                if (j > 0 && word[j] == word[j - 1] && !isUsed[j - 1]) {
-                    continue;
-                }
-                
-                arr[k] = word[j];
-                isUsed[j] = true;
-                back(k + 1, uniquePermutations);
-                isUsed[j] = false;
+        for (int i = 0; i < 26; i++) {
+            if (word[i] > 0) {
+                word[i]--;
+                arr[k] = (char) (i + 'a');
+                back(k + 1, maxSize);
+                word[i]++;
             }
         }
     }
