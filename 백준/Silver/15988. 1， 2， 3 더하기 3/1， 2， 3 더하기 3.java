1b8649c3f4bcc4ca77afd1d40c0efc4a1e71afb5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-18 13:40:51 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 128 ms, Memory: 22128 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 public class Main {
     static int N;
     static long[] dp;
+    static final long MOD = 1000000009;
+    static final int MAX = 1000000;
 
 
     public static void main(String[] args) throws Exception {
@@ -11,22 +13,21 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
+        dp = new long[MAX + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= MAX; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD;
+        }
 
         for (int k = 0; k < N; k++) {
             int num = Integer.parseInt(br.readLine());
-
-            dp = new long[Math.max(num + 1, 4)];
-
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int i = 4; i <= num; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
-            }
-
             sb.append(dp[num]).append("\n");
         }
+        
         System.out.print(sb);
     }
 }
