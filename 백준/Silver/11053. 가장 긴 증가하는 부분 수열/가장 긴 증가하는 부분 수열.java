529df03a1cc30418c1cc58b3544983576c536885
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-20 09:34:33 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,42 +4,49 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] arr;
-    static int[] dp;
+    static int[] arr, dp;
 
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        dp = new int[N];
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        
+        arr = new int[N];
+        dp = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        
+
         for (int i = 0; i < N; i++) {
             dp[i] = 1;
 
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                 }
             }
         }
 
-        
-        int result = -1;
+        System.out.print(findMax(dp));
 
-        
-        for (int i = 0; i < N; i++) {
-            result = Math.max(dp[i], result);
+    }
+
+
+    public static int findMax(int[] dp) {
+        int size = dp.length;
+        int maxNum = -1;
+
+        for (int i = 0; i < size; i++) {
+            if (dp[i] > maxNum) {
+                maxNum = dp[i];
+            }
         }
-        System.out.println(result);
+
+        return maxNum;
     }
 }
