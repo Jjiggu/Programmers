1cef323fa70d91623c3e054ab9291a08daed97fd
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-22 13:27:06 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.13 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,22 +4,16 @@ class Solution
 {
     public int solution(int []A, int []B)
     {   
-        int result = 0;
+        int sum = 0;
         
-        // 각 배열 오름차순, 내림차순 
         Arrays.sort(A);
+        Arrays.sort(B);
         
-        Integer[] tmp = Arrays.stream(B).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Collections.reverseOrder());
-        
-        
-        // 배열 돌면서 인덱스별로 계산
         for (int i = 0; i < A.length; i++) {
-            int nowSum = A[i] * tmp[i];
-            result += nowSum;
+            int nowSum = A[i] * B[B.length - (i + 1)];
+            sum += nowSum;
         }
         
-        // 계산 값 return 
-        return result;
+        return sum;
     }
 }
