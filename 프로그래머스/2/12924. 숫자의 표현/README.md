4588036cf8ad8f92544d3fc835318fe85c8bdadf
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-22 14:39:05 +0900

    [level 2] Title: 숫자의 표현, Time: 2.17 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.37 ms
+메모리: 54.6 MB, 시간: 2.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 22:12:49
+2025년 01월 22일 14:39:03
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,31 @@
+class Solution {
+    static int cnt = 0;
+    
+    public int solution(int n) {
+        dfs(1, n);
+        
+        return cnt;
+    }
+    
+    public void dfs(int start, int target) {
+        int result = 0;
+        
+        if (start == target) {
+            cnt++;
+            return;
+        }
+        
+        for (int i = start; i <= target; i++) {
+            result += i;
+            
+            if (result == target) {
+                cnt++;
+                dfs(start + 1, target);
+                return;
+            } else if (result > target) {
+                dfs(start + 1, target);
+                return;
+            }
+        }
+    }
+}
