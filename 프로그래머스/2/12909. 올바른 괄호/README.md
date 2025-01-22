b1e37616235a02dc6a0201ddd148ff7a46b07686
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-22 13:50:42 +0900

    [level 2] Title: 올바른 괄호, Time: 18.01 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 52.7 MB, 시간: 19.28 ms
+메모리: 55.3 MB, 시간: 18.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 22:12:13
+2025년 01월 22일 13:50:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
         Stack<Character> stack = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            
-            if (current == '(') {
-                stack.push(current);
-            } else if (current == ')' && stack.isEmpty()) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' && stack.isEmpty()) {
                 return false;
             } else {
                 stack.pop();
             }
         }
-    
+        
         return stack.isEmpty();
+    
     }
 }
