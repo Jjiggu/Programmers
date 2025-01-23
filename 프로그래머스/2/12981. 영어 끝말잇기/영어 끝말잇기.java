3255dcc0d5a44833e5f363147f7fd75439546c44
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-23 20:00:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,40 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int size = words.length;
-        String startWord = words[0];
         
-        int[] result = new int[2];
-        ArrayList<String> isUsed = new ArrayList<>();
+        Set<String> usedWords = new HashSet<>();
         
-        isUsed.add(words[0]);
+        usedWords.add(words[0]);
         
-        for (int i = 1; i < size; i++) {
-            isUsed.add(words[i]);
+        for (int i = 1; i < words.length; i++) {
+            String currentWord = words[i];
+            String previousWord = words[i - 1];
             
-            if (words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1) || checkMulti(words[i], isUsed)) {
-                result[0] = i % n + 1;
-                result[1] = i / n + 1;
-                
-                
-                System.out.println(i);
-                System.out.println(isUsed);
+            boolean isValidChain = currentWord.charAt(0) == previousWord.charAt(previousWord.length() - 1);
+            boolean isDuplicate = usedWords.contains(currentWord);
+            
+            if (!isValidChain || isDuplicate) {
                     
-                return result;
+                return new int[] {i % n + 1, i / n + 1};
             }    
+            
+            usedWords.add(currentWord);
         }
         
-        return result;
-    }
-    
-    
-    private boolean checkMulti(String word, ArrayList<String> wordList) {
-        for (int i = 0; i < wordList.size() - 1; i++) {
-            if (word.equals(wordList.get(i))) {
-                return true;
-            }
-        }
-        
-        return false;
+        return new int[] {0, 0};
     }
 }
