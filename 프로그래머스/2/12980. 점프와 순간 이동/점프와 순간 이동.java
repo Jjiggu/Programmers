8db44e68e7a74461b3a26013859bf76428e3a768
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-01-23 14:39:36 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,13 +1,5 @@
-import java.util.*;
-
 public class Solution {
-    public int solution(int n) {
-        // 한 번에 k 칸 앞으로 점프 -> k만큼의 건전지 소모
-        // 현재까지 온 거리 * 2 순간이동 가능 -> 건전지 소모 없음
-        String binaryNum = Integer.toBinaryString(n);
-        
-        int result = Integer.bitCount(n);
-        
-        return result;
+    public int solution(int n) {    
+        return Integer.bitCount(n);
     }
 }
