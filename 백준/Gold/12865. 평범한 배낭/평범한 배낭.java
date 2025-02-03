59b92a5a7a393fd981f76e0b90fe2604afffe193
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-02-03 14:00:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,33 +4,34 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
+    static int[] value, weight;
     static int[] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // 물품 개수
-        K = Integer.parseInt(st.nextToken()); // 최대 무게
-        dp = new int[K + 1]; // DP 테이블 (무게별 최대 가치 저장)
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        int[] weight = new int[N];
-        int[] value = new int[N];
+        weight = new int[N];
+        value = new int[N];
+        dp = new int[K + 1];
 
-        // 입력 받기
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
+
             weight[i] = Integer.parseInt(st.nextToken());
             value[i] = Integer.parseInt(st.nextToken());
         }
 
-        // **Knapsack DP 구현**
         for (int i = 0; i < N; i++) {
-            for (int w = K; w >= weight[i]; w--) { // 뒤에서부터 갱신
-                dp[w] = Math.max(dp[w], dp[w - weight[i]] + value[i]);
+            for (int j = K; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
         }
 
-        System.out.println(dp[K]); // 최대 가치 출력
+        System.out.print(dp[K]);
     }
 }
