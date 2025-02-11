a5a165be4d5c4a496d7a387a827ba7b6fa618b65
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-02-11 12:45:14 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17200 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -19,26 +19,32 @@ public class Main {
             lans[i] = Integer.parseInt(br.readLine());
         }
 
-        long low = 1;
-        long high = Arrays.stream(lans).max().getAsInt();
+        long l = 1;  // 최소 길이
+        long r = Arrays.stream(lans).max().getAsInt(); // 최대 길이
         long ans = 0;
 
-        while (low <= high) {
-            long mid = (low + high) / 2;
-            long count = 0;
+        while (l <= r) {
+            long m = (l + r) / 2;
 
-            for (int i = 0; i < K; i++) {
-                count += lans[i] / mid;
-            }
-
-            if (count >= N) { // 충분한 개수를 만들 수 있음 -> 길이를 늘려봄
-                ans = mid;
-                low = mid + 1;
-            } else { // 부족하면 길이를 줄여야 함
-                high = mid - 1;
+            if (check(m)) {  // `m` 길이로 N개 이상 만들 수 있으면
+                ans = m;  // 정답 후보 업데이트
+                l = m + 1;  // 더 긴 길이를 탐색
+            } else {
+                r = m - 1;  // 불가능하면 길이를 줄임
             }
         }
 
         System.out.print(ans);
+    }
+
+    public static boolean check(long x) {
+        long n = 0;
+
+        for (int i = 0; i < K; i++) {
+            n += lans[i] / x;
+            if (n >= N) return true;  // 필요한 개수를 넘으면 바로 종료
+        }
+
+        return false;
     }
 }
