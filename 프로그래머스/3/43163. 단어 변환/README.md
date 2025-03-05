763d9f27a9288b551bee9a74b70046e8908d1d40
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-03-05 17:52:31 +0900

    [level 3] Title: 단어 변환, Time: 1.00 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 86.7 MB, 시간: 0.27 ms
+메모리: 69 MB, 시간: 1.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 17:46:37
+2025년 03월 05일 17:52:28
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static boolean isUsed[];
-    static int cnt = 0;
-    
     public int solution(String begin, String target, String[] words) {
-        isUsed = new boolean[words.length];
+        boolean[] isUsed = new boolean[words.length];
+        Queue<Node> queue = new LinkedList<>();
         
-        int answer = dfs(0, begin, target, words, 0);
+        queue.add(new Node(begin, 0)); // 시작 단어와 변환 횟수(0) 초기화
         
-        return answer == -1 ? 0 : answer;
-    }
-    
-    public static int dfs(int idx, String nowWord, String target, String[] words, int cntNum) {
-        if (nowWord.equals(target)) return cntNum;
-        
-        int minCount = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < words.length; i++) {
-            if (!isUsed[i] && checkEquals(nowWord, words[i])) {
-                isUsed[i] = true;
-                int result = dfs(idx + 1, words[i], target, words, cntNum + 1);
-                if (result != -1) {
-                    minCount = Math.min(minCount, result);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            
+            if (current.word.equals(target)) {
+                return current.count; // 목표 단어에 도달하면 변환 횟수 반환
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                if (!isUsed[i] && checkEquals(current.word, words[i])) {
+                    isUsed[i] = true;
+                    queue.add(new Node(words[i], current.count + 1)); // 변환 후 큐에 추가
                 }
-                isUsed[i] = false;
             }
         }
         
-        return minCount == Integer.MAX_VALUE ? -1 : minCount;
+        return 0; // target에 도달하지 못한 경우 0 반환
     }
     
+    // 단어 차이가 1개 문자만 다른지 체크하는 함수
     public static boolean checkEquals(String nowWord, String comWord) {
         int cnt = 0;
-        
         for (int i = 0; i < nowWord.length(); i++) {
-            if (nowWord.charAt(i) != comWord.charAt(i)) cnt++;
+            if (nowWord.charAt(i) != comWord.charAt(i)) {
+                cnt++;
+            }
         }
+        return cnt == 1; // 한 글자만 다를 때 true
+    }
+    
+    // BFS에서 사용할 노드 클래스
+    static class Node {
+        String word;
+        int count;
         
-        return cnt == 1;
+        Node(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
     }
 }
