aa6a30f4a5d1636579ba94a5b14601f8debb6e85
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-03-19 16:51:39 +0900

    [level 3] Title: 베스트앨범, Time: 5.31 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,42 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
         
-        Map<String, Integer> genreCnt = new HashMap<>();
-        Map<String, Map<Integer, Integer>> sortedGenre = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Map<Integer, Integer>> sortedGenres = new HashMap<>();
         
         for (String genre : genres) {
-            sortedGenre.put(genre, new HashMap<>());
+            sortedGenres.put(genre, new HashMap<>());
         }
         
         for (int i = 0; i < genres.length; i++) {
-            sortedGenre.get(genres[i]).put(i, plays[i]);
+            sortedGenres.get(genres[i]).put(i, plays[i]);
         }
         
-        for (String s : sortedGenre.keySet()) {
-            genreCnt.put(s, sortedGenre.get(s).values().stream().mapToInt(n -> n).sum());
+        
+        Map<String, Integer> allPlays = new HashMap<>();
+        
+        for (String genre : sortedGenres.keySet()) {
+            allPlays.put(genre, sortedGenres.get(genre).values().stream().mapToInt(n -> n).sum());
         }
         
-        List<String> keySetList = new ArrayList<>(genreCnt.keySet());
-
-        keySetList.sort((o1, o2) -> (genreCnt.get(o2).compareTo(genreCnt.get(o1))));
         
-        keySetList.forEach(s -> {
-            Map<Integer, Integer> asdf = sortedGenre.get(s);
-
-            List<Integer> integers = new ArrayList<>(asdf.keySet());
-
-            integers.sort((o1, o2) -> (asdf.get(o2).compareTo(asdf.get(o1))));
-
-            if (integers.size() <= 1) {
-                answer.add(integers.get(0));
+        List<String> keySetList = new ArrayList<>(allPlays.keySet());
+        keySetList.sort((o1, o2) -> allPlays.get(o2).compareTo(allPlays.get(o1)));
+        
+        for (String s : keySetList) {
+            Map<Integer, Integer> maps = sortedGenres.get(s);
+            List<Integer> idxList = new ArrayList<>(maps.keySet());
+            
+            idxList.sort((o1, o2) -> maps.get(o2).compareTo(maps.get(o1)));
+            
+            if (idxList.size() <= 1) {
+                answer.add(idxList.get(0));
             } else {
-                answer.add(integers.get(0));
-                answer.add(integers.get(1));
+                answer.add(idxList.get(0));
+                answer.add(idxList.get(1));
             }
-        });
-
+            
+        }
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
