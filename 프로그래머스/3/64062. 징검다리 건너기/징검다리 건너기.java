58a4e099940753170bb1637a1bafed7f2bad5fd5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-03-25 16:27:29 +0900

    [level 3] Title: 징검다리 건너기, Time: 28.01 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,35 +1,39 @@
 class Solution {
     public int solution(int[] stones, int k) {
         int left = 1;
-        int right = 200000000; // 최대 2억
+        int right = 200_000_000;
         int answer = 0;
-
-        while (left <= right) {
+        
+        while(left <= right) {
             int mid = (left + right) / 2;
-
+            
             if (canCross(stones, k, mid)) {
                 answer = mid;
-                left = mid + 1; // 더 많이 건널 수 있는지 탐색
+                left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-
+        
         return answer;
     }
-
+    
+    
     private boolean canCross(int[] stones, int k, int mid) {
         int skip = 0;
-
+        
         for (int stone : stones) {
             if (stone < mid) {
                 skip++;
-                if (skip >= k) return false;
+                
+                if (skip >= k) {
+                    return false;
+                }
             } else {
                 skip = 0;
             }
         }
-
+        
         return true;
     }
 }
