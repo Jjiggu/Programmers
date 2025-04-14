fdfae9b664546cb81acfc5d57f515bd1ccf2e903
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-04-14 18:49:04 +0900

    [level 3] Title: 양과 늑대, Time: 4.61 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,46 +1,49 @@
 import java.util.*;
 
 class Solution {
-
-    static ArrayList<Integer>[] graph;
+    
     static int maxSheep = 0;
-
+    static ArrayList<Integer>[] graph;
+    
     public int solution(int[] info, int[][] edges) {
-        int n = info.length;
-        graph = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) {
+        
+        graph = new ArrayList[info.length];
+        
+        for (int i = 0; i < info.length; i++) {
             graph[i] = new ArrayList<>();
         }
-
-        for (int[] e : edges) {
-            graph[e[0]].add(e[1]);
+        
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
         }
-
+        
         List<Integer> nextNodes = new ArrayList<>();
         nextNodes.add(0);
-
-        dfs(0, 0, 0, nextNodes, info);
-
+        
+        dfs(0, 0, 0, nextNodes, info);   
+        
         return maxSheep;
     }
-
+    
+    
     public void dfs(int current, int sheep, int wolf, List<Integer> nextNodes, int[] info) {
-        if (info[current] == 0) sheep++;
-        else wolf++;
-
-        if (wolf >= sheep) return;  // 조건 위배 시 종료
-
-        maxSheep = Math.max(maxSheep, sheep);  // 최대값 갱신
-
+        if (info[current] == 0) {
+            sheep++;
+        } else {
+            wolf++;
+        }
+        
+        if (sheep <= wolf) return;
+        
+        maxSheep = Math.max(maxSheep, sheep);
+        
         List<Integer> candidates = new ArrayList<>(nextNodes);
-        candidates.remove(Integer.valueOf(current)); // 현재 노드 제거
-
-        // 현재 노드에서 갈 수 있는 자식 노드 추가
+        candidates.remove(Integer.valueOf(current));
+        
         for (int next : graph[current]) {
             candidates.add(next);
         }
-
+        
         for (int next : candidates) {
             dfs(next, sheep, wolf, candidates, info);
         }
