1cc4eb6025e9455e412ecdc8595045c3bc138038
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-04-24 17:53:18 +0900

    [level 3] Title: 보행자 천국, Time: 137.94 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -0,0 +1,35 @@
+class Solution {
+    static final int MOD = 20170805;
+
+    public int solution(int m, int n, int[][] cityMap) {
+        long[][][] dp = new long[m][n][2];
+        dp[0][0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cityMap[i][j] == 1) continue; 
+
+                if (j > 0) {
+                    if (cityMap[i][j - 1] == 0) {
+                        dp[i][j][0] += (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+                    } else if (cityMap[i][j - 1] == 2) {
+                        dp[i][j][0] += dp[i][j - 1][0];
+                    }
+                    dp[i][j][0] %= MOD;
+                }
+
+            
+                if (i > 0) {
+                    if (cityMap[i - 1][j] == 0) {
+                        dp[i][j][1] += (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                    } else if (cityMap[i - 1][j] == 2) {
+                        dp[i][j][1] += dp[i - 1][j][1];
+                    }
+                    dp[i][j][1] %= MOD;
+                }
+            }
+        }
+
+        return (int) ((dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD);
+    }
+}
