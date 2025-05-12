6a0c9eb591368d380997c7c880d21a002f81834f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-12 16:25:52 +0900

    [level 3] Title: 정수 삼각형, Time: 8.81 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,34 +1,19 @@
 class Solution {
-    
-    static int[][] dp;
-    
     public int solution(int[][] triangle) {
-        dp = new int[501][501];
-        dp[0][0] = triangle[0][0];
+        int[][] dp = new int[502][502];
+        int n = triangle.length;
         
-        for (int i = 1; i < triangle.length; i++) {
+        for (int i = 0; i < triangle[n - 1].length; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        
+        
+        for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j < triangle[i].length; j++) {
-                if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
-                } else if (j == triangle[i].length - 1) {
-                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];   
-                }
+                dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
         
-        return findMax(dp, triangle);
-    }
-    
-    public int findMax(int[][] dp, int[][] arr) {
-        int maxLen = arr.length - 1;
-        int maxNum = -1;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (dp[maxLen][i] > maxNum) maxNum = dp[maxLen][i];
-        }
-        
-        return maxNum;
+        return dp[0][0];
     }
 }
