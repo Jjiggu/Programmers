b5a0372a5ebdb345437163241700e760cf719e57
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-14 16:51:43 +0900

    [level 3] Title: 야근 지수, Time: 125.49 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,28 +1,38 @@
 import java.util.*;
 
 class Solution {
+    static PriorityQueue<Integer> pq;
+    
     public long solution(int n, int[] works) {
+        pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        long answer = 0;
+        for (int work : works) pq.add(work);
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int work : works) {
-            pq.offer(work);
-        }
-
-        while (n > 0) {
-            int num = pq.poll();
+        doOvertime(n);
+        
+        return pqSum(pq);
+    }
+    
+    
+    public void doOvertime(int n) {
+        
+        for (int i = 0; i < n; i++) {
+            int nowNum = pq.poll();
+            nowNum--;
             
-            pq.offer(num > 0 ? num - 1 : num);
-            n--;
+            if (nowNum > 0) pq.offer(nowNum);
+            else pq.offer(0);
         }
-
-        for (int num : pq) {
-            answer += Math.pow(num, 2);
-        }
-
-        return answer;
+    }
+    
+    
+    private long pqSum(PriorityQueue<Integer> pq) {
+        long result = 0;
         
+        while(!pq.isEmpty()) {
+            result += Math.pow(pq.poll(), 2);
+        }
+        
+        return result;
     }
 }
