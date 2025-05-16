73ef75c2687b13cc25fbbec949644d40ad4a567f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-16 11:31:27 +0900

    [level 3] Title: 단어 변환, Time: 0.47 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,48 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        
         boolean[] isUsed = new boolean[words.length];
-        Queue<Node> queue = new LinkedList<>();
         
-        queue.add(new Node(begin, 0)); // 시작 단어와 변환 횟수(0) 초기화
+        int result = dfs(0, begin, target, isUsed, words);
         
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            
-            if (current.word.equals(target)) {
-                return current.count; // 목표 단어에 도달하면 변환 횟수 반환
-            }
-            
-            for (int i = 0; i < words.length; i++) {
-                if (!isUsed[i] && checkEquals(current.word, words[i])) {
-                    isUsed[i] = true;
-                    queue.add(new Node(words[i], current.count + 1)); // 변환 후 큐에 추가
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+    
+    
+    public int dfs(int k, String nowWord, String target, boolean[] isUsed, String[] words) {
+        
+        if (nowWord.equals(target)) {
+            return k;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < words.length; i++) {
+            if (!isUsed[i] && isMatched(nowWord, words[i])) {
+                isUsed[i] = true;
+                int val = dfs(k + 1, words[i], target, isUsed, words);
+                
+                if (val < min) {
+                    min = val;
                 }
+                
+                isUsed[i] = false;
             }
         }
         
-        return 0; // target에 도달하지 못한 경우 0 반환
+        return min;
     }
     
-    // 단어 차이가 1개 문자만 다른지 체크하는 함수
-    public static boolean checkEquals(String nowWord, String comWord) {
+    
+    public boolean isMatched(String word, String target) {
         int cnt = 0;
-        for (int i = 0; i < nowWord.length(); i++) {
-            if (nowWord.charAt(i) != comWord.charAt(i)) {
-                cnt++;
-            }
-        }
-        return cnt == 1; // 한 글자만 다를 때 true
-    }
-    
-    // BFS에서 사용할 노드 클래스
-    static class Node {
-        String word;
-        int count;
         
-        Node(String word, int count) {
-            this.word = word;
-            this.count = count;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != target.charAt(i)) cnt++;
         }
+        
+        return cnt == 1;
     }
 }
