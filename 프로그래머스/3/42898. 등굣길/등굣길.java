cf5e418bd631e044d6aed1ed19761233f7dbf346
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-16 13:37:21 +0900

    [level 3] Title: 등굣길, Time: 1.17 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,31 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static int[][] dp;
-    static boolean[][] visited;
-    static int MOD = 1_000_000_007;
-     
+    static final int MOD = 1_000_000_007;
+    
     public int solution(int m, int n, int[][] puddles) {
-        
-        visited = new boolean[n + 2][m + 2];
+        int[][] dp = new int[m + 3][n + 3];
         
         for (int[] puddle : puddles) {
-            visited[puddle[1]][puddle[0]] = true;
+            int x = puddle[0];
+            int y = puddle[1];
+            dp[x][y] = -1;
         }
         
-        dp = new int[n + 2][m + 2];
-        dp[1][1] = 1;
-        visited[1][1] = true;
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (!visited[i][j]) {
-                    dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD; 
+        for (int i = 0; i <= m + 2; i++) {
+            for (int j = 0; j <= n + 2; j++) {
+                if (i == 0 || i == m + 2 || j == 0 || j == n + 2) {
+                    dp[i][j] = -1;
                 }
+             }
+        }
+        
+        dp[1][1] = 1;
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) continue;
+                if (dp[i][j] == -1) continue;
+                
+                int fromTop = dp[i - 1][j] == -1 ? 0 : dp[i - 1][j];
+                int fromLeft = dp[i][j - 1] == -1 ? 0 : dp[i][j - 1];
+                dp[i][j] = (fromTop + fromLeft) % MOD;
             }
         }
         
-        return dp[n][m];
-            
+        return dp[m][n];
     }    
 }
