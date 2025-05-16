5f4c10d179fde929232a05ebac2505e782dc6faa
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-16 13:42:51 +0900

    [level 3] Title: 등굣길, Time: 0.87 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -12,24 +12,17 @@ class Solution {
             dp[x][y] = -1;
         }
         
-        for (int i = 0; i <= m + 2; i++) {
-            for (int j = 0; j <= n + 2; j++) {
-                if (i == 0 || i == m + 2 || j == 0 || j == n + 2) {
-                    dp[i][j] = -1;
-                }
-             }
-        }
-        
         dp[1][1] = 1;
         
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == 1 && j == 1) continue;
-                if (dp[i][j] == -1) continue;
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0; 
+                    continue;
+                }
                 
-                int fromTop = dp[i - 1][j] == -1 ? 0 : dp[i - 1][j];
-                int fromLeft = dp[i][j - 1] == -1 ? 0 : dp[i][j - 1];
-                dp[i][j] = (fromTop + fromLeft) % MOD;
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+                dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
             }
         }
         
