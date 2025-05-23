93389e0b2f74997dfe29a8efdf9d4937fd7e6112
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-23 11:44:29 +0900

    [level 3] Title: 불량 사용자, Time: 116.43 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,44 +1,42 @@
 import java.util.*;
 
 class Solution {
-    static HashSet<HashSet<String>> answer = new HashSet<>();
-    
     public int solution(String[] user_id, String[] banned_id) {
-        HashSet<String> set = new HashSet<>();
+        Set<Set<String>> answers = new HashSet<>();
         
-        dfs(0, user_id, banned_id, set);
+        dfs(0, user_id, banned_id, new HashSet<>(), answers);
         
-        return answer.size();
+        return answers.size();
     }
 
     
-    public void dfs(int k, String[] user_id, String[] banned_id, HashSet<String> set) {
-        if (k == banned_id.length) {
-            if (set.size() == banned_id.length) {
-                answer.add(new HashSet<>(set)); 
+    public void dfs(int depth, String[] user_id, String[] banned_id, Set<String> currentSet, Set<Set<String>> answers) {
+        if (depth == banned_id.length) {
+            if (currentSet.size() == banned_id.length) {
+                answers.add(new HashSet<>(currentSet)); 
             }
             return;
         }
         
-        for (int i = 0; i < user_id.length; i++) {
-            if (set.contains(user_id[i])) continue;
+        for (String user : user_id) {
+            if (currentSet.contains(user)) continue;
             
-            if(isValid(user_id[i], banned_id[k])) {
-                set.add(user_id[i]);
-                dfs(k + 1, user_id, banned_id, set);
-                set.remove(user_id[i]);
+            if(isMatch(user, banned_id[depth])) {
+                currentSet.add(user);
+                dfs(depth + 1, user_id, banned_id, currentSet, answers);
+                currentSet.remove(user);
             }
         }
     }
 
         
-    public boolean isValid(String user, String bannedUser) {
+    public boolean isMatch(String user, String banned) {
         
-        if (user.length() != bannedUser.length()) return false;
+        if (user.length() != banned.length()) return false;
         
         for (int i = 0; i < user.length(); i++) {
-            if (bannedUser.charAt(i) == '*') continue;
-            if (user.charAt(i) != bannedUser.charAt(i)) return false;
+            if (banned.charAt(i) == '*') continue;
+            if (user.charAt(i) != banned.charAt(i)) return false;
         }
         
         return true;
