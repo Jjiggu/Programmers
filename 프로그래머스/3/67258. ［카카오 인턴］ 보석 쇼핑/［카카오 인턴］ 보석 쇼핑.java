3f8e2303fbec752a240bc1c20000f7115ebcc174
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-23 12:29:58 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.70 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,25 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
-        int gemTypes = new HashSet<>(Arrays.asList(gems)).size(); 
-        int length = Integer.MAX_VALUE, start = 0;
-        Map<String, Integer> map = new HashMap<>();
+        int gemTypes = new HashSet<>(Arrays.asList(gems)).size();
+        int left = 0, right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int answerLeft = 0, answerRight = 0;
         
-        for (int i = 0; i < gems.length; i++) {
-            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
-            
-            while (map.get(gems[start]) > 1) {
-                map.put(gems[start], map.get(gems[start]) - 1);
-                start++;
-            }
-            
-            if (map.size() == gemTypes && length > (i - start)) {
-                length = i - start;
-                answer[0] = start + 1;
-                answer[1] = i + 1;
+        Map<String, Integer> gemList = new HashMap<>();
+        
+        
+        while (true) {
+            if (gemList.size() < gemTypes) {
+                if (right == gems.length) break;
+                
+                gemList.put(gems[right], gemList.getOrDefault(gems[right], 0) + 1);
+                right++;
+            } else {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    answerLeft = left;
+                    answerRight = right;
+                }
+                
+                gemList.put(gems[left], gemList.get(gems[left]) - 1);
+                
+                if (gemList.get(gems[left]) == 0) {
+                    gemList.remove(gems[left]);
+                }
+                left++;
             }
         }
-        return answer;
+        
+        return new int[] {answerLeft + 1, answerRight};
     }
 }
