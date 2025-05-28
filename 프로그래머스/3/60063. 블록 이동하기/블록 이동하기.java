3ee67380a7a578e26169da321323a05bba1eb64b
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-05-28 19:19:58 +0900

    [level 3] Title: 블록 이동하기, Time: 15.40 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -2,120 +2,99 @@ import java.util.*;
 
 class Solution {
 
-    static class Block {
-        int x1, y1, x2, y2, time;
-
-        Block(int x1, int y1, int x2, int y2, int time) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
+    static class State {
+        int x, y, dir, time;
+        State(int x, int y, int dir, int time) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;  
             this.time = time;
         }
-
-        
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Block)) return false;
-            Block b = (Block) o;
-            return ((x1 == b.x1 && y1 == b.y1 && x2 == b.x2 && y2 == b.y2) ||
-                    (x1 == b.x2 && y1 == b.y2 && x2 == b.x1 && y2 == b.y1));
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x1 + x2, y1 + y2); 
-        }
     }
+
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
 
     public int solution(int[][] board) {
         int n = board.length;
-        Queue<Block> queue = new LinkedList<>();
-        Set<Block> visited = new HashSet<>();
+        boolean[][][] visited = new boolean[n][n][2];
+        Queue<State> queue = new LinkedList<>();
 
-        Block start = new Block(0, 0, 0, 1, 0);
-        queue.offer(start);
-        visited.add(start);
+        queue.offer(new State(0, 0, 0, 0));
+        visited[0][0][0] = true;
 
         while (!queue.isEmpty()) {
-            Block cur = queue.poll();
+            State cur = queue.poll();
+            int x = cur.x, y = cur.y, dir = cur.dir, t = cur.time;
 
-            
-            if ((cur.x1 == n - 1 && cur.y1 == n - 1) || (cur.x2 == n - 1 && cur.y2 == n - 1)) {
-                return cur.time;
+            int x2 = x + (dir == 0 ? 0 : 1);
+            int y2 = y + (dir == 0 ? 1 : 0);
+
+            if ((x == n - 1 && y == n - 1) || (x2 == n - 1 && y2 == n - 1)) {
+                return t;
             }
 
-            
-            for (int[] next : move(cur, board)) {
-                Block nextBlock = new Block(next[0], next[1], next[2], next[3], cur.time + 1);
-                if (!visited.contains(nextBlock)) {
-                    visited.add(nextBlock);
-                    queue.offer(nextBlock);
+            for (int i = 0; i < 4; i++) {
+                int nx1 = x + dx[i];
+                int ny1 = y + dy[i];
+                int nx2 = x2 + dx[i];
+                int ny2 = y2 + dy[i];
+                
+                if (isIn(nx1, ny1, n) && isIn(nx2, ny2, n)
+                 && board[nx1][ny1] == 0 && board[nx2][ny2] == 0
+                 && !visited[nx1][ny1][dir]) {
+
+                    visited[nx1][ny1][dir] = true;
+                    queue.offer(new State(nx1, ny1, dir, t + 1));
                 }
             }
-
-            
-            for (int[] next : rotate(cur, board)) {
-                Block nextBlock = new Block(next[0], next[1], next[2], next[3], cur.time + 1);
-                if (!visited.contains(nextBlock)) {
-                    visited.add(nextBlock);
-                    queue.offer(nextBlock);
-                }
-            }
-        }
-
-        return -1; 
-    }
 
     
-    private List<int[]> move(Block b, int[][] board) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        int n = board.length;
-        List<int[]> result = new ArrayList<>();
+            if (dir == 0) {  
+                for (int d : new int[]{-1, 1}) {
+                    int nx = x + d;
+                    
+                    if (!isIn(nx, y, n) || !isIn(nx, y + 1, n)
+                     || board[nx][y] != 0 || board[nx][y + 1] != 0) continue;
 
-        for (int i = 0; i < 4; i++) {
-            int nx1 = b.x1 + dx[i];
-            int ny1 = b.y1 + dy[i];
-            int nx2 = b.x2 + dx[i];
-            int ny2 = b.y2 + dy[i];
+                    int baseX = Math.min(x, nx);
+                    
+                    if (!visited[baseX][y][1]) {
+                        visited[baseX][y][1] = true;
+                        queue.offer(new State(baseX, y, 1, t + 1));
+                    }
+                    
+                    if (!visited[baseX][y + 1][1]) {
+                        visited[baseX][y + 1][1] = true;
+                        queue.offer(new State(baseX, y + 1, 1, t + 1));
+                    }
+                }
+            } else {        
+                for (int d : new int[]{-1, 1}) {
+                    int ny = y + d;
+                    
+                    if (!isIn(x, ny, n) || !isIn(x + 1, ny, n)
+                     || board[x][ny] != 0 || board[x + 1][ny] != 0) continue;
 
-            if (isFree(nx1, ny1, board) && isFree(nx2, ny2, board)) {
-                result.add(new int[]{nx1, ny1, nx2, ny2});
-            }
-        }
-
-        return result;
-    }
-
-    
-    private List<int[]> rotate(Block b, int[][] board) {
-        List<int[]> result = new ArrayList<>();
-        int n = board.length;
-
-        if (b.x1 == b.x2) {
-            for (int d = -1; d <= 1; d += 2) {
-                if (isFree(b.x1 + d, b.y1, board) && isFree(b.x2 + d, b.y2, board)) {
-                    result.add(new int[]{b.x1, b.y1, b.x1 + d, b.y1});
-                    result.add(new int[]{b.x2, b.y2, b.x2 + d, b.y2});
+                    int baseY = Math.min(y, ny);
+                    
+                    if (!visited[x][baseY][0]) {
+                        visited[x][baseY][0] = true;
+                        queue.offer(new State(x, baseY, 0, t + 1));
+                    }
+                    
+                    if (!visited[x + 1][baseY][0]) {
+                        visited[x + 1][baseY][0] = true;
+                        queue.offer(new State(x + 1, baseY, 0, t + 1));
+                    }
                 }
             }
         }
-        
-        else if (b.y1 == b.y2) {
-            for (int d = -1; d <= 1; d += 2) {
-                if (isFree(b.x1, b.y1 + d, board) && isFree(b.x2, b.y2 + d, board)) {
-                    result.add(new int[]{b.x1, b.y1, b.x1, b.y1 + d});
-                    result.add(new int[]{b.x2, b.y2, b.x2, b.y2 + d});
-                }
-            }
-        }
 
-        return result;
+        return -1;
     }
 
-    private boolean isFree(int x, int y, int[][] board) {
-        int n = board.length;
-        return (x >= 0 && y >= 0 && x < n && y < n && board[x][y] == 0);
+    private boolean isIn(int x, int y, int n) {
+        return x >= 0 && y >= 0 && x < n && y < n;
     }
 }
