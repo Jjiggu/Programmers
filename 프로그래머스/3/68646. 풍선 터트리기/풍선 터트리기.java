063cc47656d285cca69a3fb5c298b6bef3b5aad6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-01 18:21:49 +0900

    [level 3] Title: 풍선 터트리기, Time: 38.12 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -2,31 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] a) {
+        int answer = 2;
         int n = a.length;
-        int[] leftMin = new int[n];
-        int[] rightMin = new int[n];
+        int[] leftMin = buildAccumulatedMin(a, "left");
+        int[] rightMin = buildAccumulatedMin(a, "right");
         
-        leftMin[0] = a[0];
-        for (int i = 1; i < n; i++) {
-            leftMin[i] = Math.min(leftMin[i - 1], a[i]);
+        
+        for (int i = 1; i < a.length - 1; i++) {
+            int nowNum = a[i];
+            
+            if (a[i] > leftMin[i - 1] && a[i] > rightMin[i + 1]) continue;
+            
+            answer++;
         }
         
-        rightMin[n - 1] = a[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMin[i] = Math.min(rightMin[i + 1], a[i]);
-        }
+        return answer;
+    }
+    
+    
+    private int[] buildAccumulatedMin(int[] arr, String direction) {
+        int n = arr.length;
+        int[] minArr = new int[n];
         
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 | i == n - 1) {
-                cnt++;
-            } else {
-                if (a[i] < leftMin[i - 1] || a[i] < rightMin[i + 1]) {
-                    cnt++;
-                }
+        if ("left".equals(direction)) {
+            minArr[0] = arr[0];
+            for (int i = 1; i < n; i++) {
+                minArr[i] = Math.min(minArr[i - 1], arr[i]);
+            }
+        } else if ("right".equals(direction)) {
+            minArr[n - 1] = arr[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                minArr[i] = Math.min(minArr[i + 1], arr[i]);
             }
         }
         
-        return cnt;
+        return minArr;
     }
 }
