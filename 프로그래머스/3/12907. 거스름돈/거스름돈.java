84320ec5442ec2cc1efd16d8870e38f779ac5590
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-01 16:57:19 +0900

    [level 3] Title: 거스름돈, Time: 15.68 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,18 +1,17 @@
 class Solution {
     
-    static int MOD = 1_000_000_007;
+    static final int MOD = 1_000_000_007;
     
     public int solution(int n, int[] money) {
-        int[] dp = new int[n + 1];
+        int[] dp = new int[100001];
         dp[0] = 1;
         
-        
         for (int coin : money) {
-            for (int i = coin; i <= n; i++) {
-                dp[i] = (dp[i] + dp[i - coin]) % MOD;
+            for (int i = coin; i <= n; i++) {    
+                dp[i] += dp[i - coin] % MOD;    
             }
         }
-        
-        return dp[n];  
+    
+        return dp[n];
     }
 }
