028918ab6f82c3c1589a6754b5983fd2abd26a79
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-01 16:35:32 +0900

    [level 3] Title: 입국심사, Time: 41.58 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,7 +2,6 @@ class Solution {
     public long solution(int n, int[] times) {
         long right = 1_000_000_000L * 100_000L;
         long left = 0;
-        long answer = Long.MAX_VALUE;
         
         while(left <= right) {
             long mid = (left + right) / 2;
@@ -13,13 +12,12 @@ class Solution {
             }
             
             if (total >= n) {
-                answer = Math.min(answer, mid);
                 right = mid - 1;
             } else {
                 left = mid + 1;
             } 
         }
         
-        return answer;
+        return left;
     }
 }
