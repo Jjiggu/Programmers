10fed635bb736b16bba839047fabf4940300f557
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-01 19:43:15 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 72.32 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        Map<String, String> maps = new HashMap<>();
-        Map<String, Integer> revenue = new HashMap<>();
+        
         int[] answer = new int[enroll.length];
+        Map<String, String> group = new HashMap<>();
+        Map<String, Integer> profit = new HashMap<>();
         
         for (int i = 0; i < enroll.length; i++) {
-            maps.put(enroll[i], referral[i]);
-            revenue.put(enroll[i], revenue.getOrDefault(enroll[i], 0));
+            group.put(enroll[i], referral[i]);
+            profit.put(enroll[i], profit.getOrDefault(enroll[i], 0));
         }
         
         
         for (int i = 0; i < amount.length; i++) {
-            String curr = seller[i];
-            int sell = amount[i] * 100;
+            int price = amount[i] * 100;
+            String sellerName = seller[i];
             
-            while (!curr.equals("-") && sell > 0) {
-                int ref = sell / 10;
-                int keep = sell - ref;
+            while (!sellerName.equals("-") && price > 0) {
+                int refProfit = price / 10;
+                int myProfit = price - refProfit;   
                 
-                revenue.put(curr, revenue.get(curr) + keep);
+                profit.put(sellerName, profit.get(sellerName) + myProfit);
                 
-                curr = maps.get(curr);
-                sell = ref;
+                sellerName = group.get(sellerName);
+                price = refProfit;  
             }
         }
         
-        
         for (int i = 0; i < enroll.length; i++) {
-            answer[i] = revenue.get(enroll[i]);
+            answer[i] = profit.get(enroll[i]);
         }
         
         return answer;
