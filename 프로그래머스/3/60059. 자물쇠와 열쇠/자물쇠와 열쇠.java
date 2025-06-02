cd9cd310e3a09c410483f761b59af091ed3a52c3
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-02 14:12:32 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 23.00 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,41 +1,40 @@
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
+        int keyLen = key.length;
+        int lockLen = lock.length;
+        int mapLen = lockLen + 2 * (keyLen - 1);
         
-        int M = key.length;
-        int N = lock.length;
-        
-        int size = N + 2 * (M - 1);
-        int[][] map = new int[size][size];
-        
-        for (int i = 0 ; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                map[i + M - 1][j + M - 1] = lock[i][j];
+        int[][] map = new int[mapLen][mapLen];
+        for (int i = 0; i < lockLen; i++) {
+            for (int j = 0; j < lockLen; j++) {
+                map[i + keyLen - 1][j + keyLen - 1] = lock[i][j];
             }
         }
         
-        
-        for (int r = 0; r < 4; r++) {
+        for (int rot = 0; rot < 4; rot++) {
             key = rotate(key);
             
-            for (int x = 0; x <= size - M; x++) {
-                for (int y = 0; y <= size - M; y++) {
-                    int[][] tmp = deepCopy(map);
-                    
-                    for (int i = 0; i < M; i++) {
-                        for (int j = 0; j < M; j++) {
-                            tmp[x + i][y + j] += key[i][j];
+            for (int x = 0; x <= mapLen - keyLen; x++) {
+                for (int y = 0; y <= mapLen - keyLen; y++) {
+                    for (int i = 0; i < keyLen; i++) {
+                        for (int j = 0; j < keyLen; j++) {
+                            map[x + i][y + j] += key[i][j];
                         }
                     }
                     
-                    if (checkLock(tmp, M - 1, N)) return true;
+                    if (checkLock(map, keyLen - 1, lockLen)) return true;
+                    
+                    for (int i = 0; i < keyLen; i++) {
+                        for (int j = 0; j < keyLen; j++) {
+                            map[x + i][y + j] -= key[i][j];
+                        }
+                    }
                 }
             }
         }
         
         return false;
-        
     }
-    
     
     private int[][] rotate(int[][] key) {
         int n = key.length;
@@ -43,32 +42,19 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                rotated[i][j] = key[n - 1 - j][i];
+                rotated[n - 1 - j][i] = key[i][j];
             }
         }
         return rotated;
     }
     
-    
-    private boolean checkLock(int[][] map, int offset, int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++){
-                if (map[offset + i][offset + j] != 1) {
-                    return false;
-                }
+    private boolean checkLock(int[][] map, int offset, int lockLen) {
+        for (int i = 0; i < lockLen; i++) {
+            for (int j = 0; j < lockLen; j++) {
+                if (map[i + offset][j + offset] != 1) return false;
             }
         }
+        
         return true;
-    }
-    
-    
-    private int[][] deepCopy(int[][] arr) {
-        int[][] copy = new int[arr.length][arr[0].length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            copy[i] = arr[i].clone();
-        }
-        
-        return copy;
     }
 }
