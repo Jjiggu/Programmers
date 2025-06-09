0b3eeb2e3944c37527bd0b2d17e22b96ac05473a
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-09 15:11:39 +0900

    [level 3] Title: 광고 삽입, Time: 318.56 ms, Memory: 288 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -4,32 +4,29 @@ class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
         int playTime = timeToSec(play_time.split(":"));
         int advTime = timeToSec(adv_time.split(":"));
-
+        
         long[] timeline = new long[playTime + 2];
-
+        
         for (String log : logs) {
             String[] tmp = log.split("-");
             int start = timeToSec(tmp[0].split(":"));
             int end = timeToSec(tmp[1].split(":"));
-
+            
             timeline[start] += 1;
-            timeline[end] -= 1;
+            timeline[end] -=1;
         }
-
         
         for (int i = 1; i <= playTime; i++) {
             timeline[i] += timeline[i - 1];
         }
-
         
         for (int i = 1; i <= playTime; i++) {
             timeline[i] += timeline[i - 1];
         }
-
         
         long maxView = timeline[advTime - 1];
         int startTime = 0;
-
+        
         for (int i = advTime; i <= playTime; i++) {
             long currentView = timeline[i] - timeline[i - advTime];
             if (currentView > maxView) {
@@ -37,27 +34,25 @@ class Solution {
                 startTime = i - advTime + 1;
             }
         }
-
+        
         return secToTime(startTime);
     }
     
-
-    public int timeToSec(String[] timeArr) {
+    private int timeToSec(String[] timeArr) {
         int[] units = {3600, 60, 1};
         int sec = 0;
         for (int i = 0; i < timeArr.length; i++) {
             sec += Integer.parseInt(timeArr[i]) * units[i];
         }
         return sec;
-    }
+    } 
     
-
-    public String secToTime(int sec) {
+    private String secToTime(int sec) {
         int hour = sec / 3600;
         sec %= 3600;
         int min = sec / 60;
         sec %= 60;
-
+        
         return String.format("%02d:%02d:%02d", hour, min, sec);
     }
 }
