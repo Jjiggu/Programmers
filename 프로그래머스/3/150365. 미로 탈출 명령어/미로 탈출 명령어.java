8370bfb8aa22d5a64d0aade35ee66b208f536b85
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-09 14:47:14 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.18 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,35 +1,35 @@
+import java.util.*;
+
 class Solution {
-
-    static int[] dx = {1, 0, 0, -1};             // d, l, r, u
-    static int[] dy = {0, -1, 1, 0};
-    static String[] direction = {"d", "l", "r", "u"};
-
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-
+        int[] dx = {1, 0, 0, -1};             // d, l, r, u
+        int[] dy = {0, -1, 1, 0};
+        String[] direction = {"d", "l", "r", "u"};
+        
         int dist = Math.abs(x - r) + Math.abs(y - c);
         if (dist > k || (k - dist) % 2 != 0) return "impossible";
-
-        StringBuilder path = new StringBuilder();
-
+        
+        StringBuilder sb = new StringBuilder();
+        
         for (int step = 0; step < k; step++) {
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
-                if (nx < 1 || ny < 1 || nx > n || ny > m) continue;
-
-                int restDist = Math.abs(nx - r) + Math.abs(ny - c);
-                int restStep = k - step - 1;
-
-                if (restDist <= restStep && (restStep - restDist) % 2 == 0) {
-                    path.append(direction[i]);
+                
+                if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+                
+                int remainDist = Math.abs(nx - r) + Math.abs(ny - c);
+                int remainStep = k - step - 1;
+                
+                if (remainDist <= remainStep && (remainStep - remainDist) % 2 == 0) {
+                    sb.append(direction[i]);
                     x = nx;
                     y = ny;
-                    break;  // 방향 하나 결정되면 다음 step
+                    break;
                 }
             }
         }
-
-        return path.toString();
+        
+        return sb.toString();
     }
 }
