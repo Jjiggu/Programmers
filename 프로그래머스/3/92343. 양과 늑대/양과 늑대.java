171045253a2b889c3f8effdf4bdd766bd0a01045
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-09 12:30:11 +0900

    [level 3] Title: 양과 늑대, Time: 5.45 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,51 +1,37 @@
 import java.util.*;
 
 class Solution {
-    
-    static int maxSheep = 0;
-    static ArrayList<Integer>[] graph;
+    int maxSheep = 0;
     
     public int solution(int[] info, int[][] edges) {
-        
-        graph = new ArrayList[info.length];
-        
-        for (int i = 0; i < info.length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
-        }
+        List<Integer>[] children = new ArrayList[info.length];
+        for (int i = 0; i < info.length; i++) children[i] = new ArrayList<>();
+        for (int[] edge : edges) children[edge[0]].add(edge[1]);
         
         List<Integer> nextNodes = new ArrayList<>();
         nextNodes.add(0);
-        
-        dfs(0, 0, 0, nextNodes, info);   
+        dfs(0, 0, 0, nextNodes, info, children);
         
         return maxSheep;
     }
     
-    
-    public void dfs(int current, int sheep, int wolf, List<Integer> nextNodes, int[] info) {
-        if (info[current] == 0) {
-            sheep++;
-        } else {
-            wolf++;
-        }
+    public void dfs(int curr, int sheep, int wolf, List<Integer> nextNodes, int[] info, List<Integer>[] children) {
+        if (info[curr] == 0) sheep++;
+        else wolf++;
         
-        if (sheep <= wolf) return;
+        if (wolf >= sheep) return;
         
         maxSheep = Math.max(maxSheep, sheep);
         
-        List<Integer> candidates = new ArrayList<>(nextNodes);
-        candidates.remove(Integer.valueOf(current));
+        List<Integer> canVisit = new ArrayList<>(nextNodes);
+        canVisit.remove((Integer)curr);
         
-        for (int next : graph[current]) {
-            candidates.add(next);
+        for (int next : children[curr]) {
+            canVisit.add(next);
         }
         
-        for (int next : candidates) {
-            dfs(next, sheep, wolf, candidates, info);
+        for (int next : canVisit) {
+            dfs(next, sheep, wolf, canVisit, info, children);
         }
     }
 }
