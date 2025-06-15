67dc5f592b73325a78acbbe705f9037fbc593614
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-16 00:38:05 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 35.68 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,44 +1,37 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(long[] numbers) {
-        
         int[] answer = new int[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
             String binary = Long.toBinaryString(numbers[i]);
-            String tree = setLength(binary);
+            String tree = setLenth(binary);
             
             answer[i] = isValid(tree) ? 1 : 0;
         }
         
-        
         return answer;
     }
     
-    
-    public String setLength(String binary) {
-        int length = binary.length();
+    private String setLenth(String binary) {
+        int len = binary.length();
         int treeHeight = 0;
         
-        while((Math.pow(2, treeHeight) - 1) < length) {
+        while((Math.pow(2, treeHeight) - 1) < len) {
             treeHeight++;
         }
         
-        int fullLength = (int)Math.pow(2, treeHeight) - 1;
-        int zeroPadding = fullLength - length;
+        int fullLen = (1 << treeHeight) - 1;
+        int zeropadding = fullLen - len;
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < zeroPadding; i++) sb.append("0");
+        for (int i = 0; i < zeropadding; i++) sb.append("0");
         sb.append(binary);
         
         return sb.toString();
     }
     
-    
-    public boolean isValid(String tree) {
-        
-        if (tree.length() == 1) return true;
+    private boolean isValid(String tree) {
+        if(tree.length() == 1) return true;
         
         int mid = tree.length() / 2;
         char root = tree.charAt(mid);
@@ -47,9 +40,7 @@ class Solution {
         String right = tree.substring(mid + 1);
         
         if (root == '0') {
-            if (left.charAt(left.length() / 2) == '1' || right.charAt(right.length() / 2) == '1') {
-                return false;
-            }
+            if (left.charAt(left.length() / 2) == '1' || right.charAt(right.length() / 2) == '1') return false;
         }
         
         return isValid(left) && isValid(right);
