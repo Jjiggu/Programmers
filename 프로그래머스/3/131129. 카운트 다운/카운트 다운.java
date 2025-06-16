fcf0360bb73dbeb6390e0c72c3298e79b56e622f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-16 12:01:25 +0900

    [level 3] Title: 카운트 다운, Time: 104.63 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -2,42 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int target) {
-        // 1. 만들 수 있는 모든 점수 집합
-        Set<Integer> darts = new HashSet<>();
+        Set<Integer> scores = new HashSet<>();
         for (int i = 1; i <= 20; i++) {
-            darts.add(i);        // 싱글
-            darts.add(i * 2);    // 더블
-            darts.add(i * 3);    // 트리플
+            scores.add(i);
+            scores.add(i * 2);
+            scores.add(i * 3);
         }
-        darts.add(50); // 불
-
-        // 2. 싱글/불만 따로 집합(마지막 한 발 체크용)
+        scores.add(50);
+        
         Set<Integer> singleBull = new HashSet<>();
         for (int i = 1; i <= 20; i++) singleBull.add(i);
         singleBull.add(50);
-
-        // 3. DP 테이블 [0: 최소 다트 개수, 1: 싱글/불 사용 개수]
-        int[][] dp = new int[target + 1][2];
+        
+        
+        int dp[][] = new int[target + 1][2];
         for (int i = 1; i <= target; i++) dp[i][0] = Integer.MAX_VALUE;
-        // dp[0] = {0, 0}
-
-        // 4. DP 진행
+        
         for (int i = 1; i <= target; i++) {
-            for (int d : darts) {
-                if (i - d < 0) continue;
-                int prevDarts = dp[i - d][0] + 1;
-                int prevSingleBull = dp[i - d][1] + (singleBull.contains(d) ? 1 : 0);
-
-                // 최소 다트 개수 우선, 같으면 싱글/불 최대 우선
+            for (int dart : scores) {
+                if (i - dart < 0) continue;
+                int prevDarts = dp[i - dart][0] + 1;
+                int prevSingleBull = dp[i - dart][1] + (singleBull.contains(dart) ? 1 : 0);
+                
                 if (prevDarts < dp[i][0]) {
                     dp[i][0] = prevDarts;
                     dp[i][1] = prevSingleBull;
-                } else if (prevDarts == dp[i][0]) {
+                } else if (prevDarts == dp[i][0]){
                     dp[i][1] = Math.max(dp[i][1], prevSingleBull);
                 }
+                
             }
         }
-
+        
         return dp[target];
     }
 }
