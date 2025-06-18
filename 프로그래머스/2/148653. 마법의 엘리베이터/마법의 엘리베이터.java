45a8e99803b5e3133dc4f74b8f09eed8255b02a7
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-18 15:34:48 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -6,18 +6,11 @@ class Solution {
             int lastDigit = storey % 10;
             int next = (storey / 10) % 10;
             
-            if (lastDigit > 5) {
-                answer += (10 - lastDigit);
-                storey += 10;
-            } else if (lastDigit < 5) {
-                answer += lastDigit;
+            if (lastDigit > 5 || (lastDigit == 5 && next >= 5)) {
+                answer += 10 - lastDigit;
+                storey += 10 - lastDigit;
             } else {
-                if (next >= 5) {
-                    answer += 5;
-                    storey += 10;
-                } else {
-                    answer += 5;
-                }
+                answer += lastDigit;
             }
             storey /= 10;
         }
