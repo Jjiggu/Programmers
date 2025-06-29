249e32d529cf06b3d8a13cf0f34fce85e3e78604
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-06-29 17:49:38 +0900

    [level 3] Title: 등대, Time: 87.67 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] lighthouse) {
-        
         List<Integer>[] graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
         
         for (int[] edge : lighthouse) {
-            int u = edge[0], v = edge[1];
-            graph[v].add(u);
+            int u = edge[0];
+            int v = edge[1];
             graph[u].add(v);
+            graph[v].add(u);
         }
         
         int dp[][] = new int[n + 1][2];
-        
         dfs(1, 0, graph, dp);
         
-        int answer = Math.min(dp[1][0], dp[1][1]);
-        return answer;
+        return Math.min(dp[1][0], dp[1][1]);
     }
     
     private void dfs(int node, int parent, List<Integer>[] graph, int[][] dp) {
