e1d3ab0dde4a9600db803034a5beb62f6c0fec39
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-07-07 13:40:55 +0900

    [level 2] Title: 점 찍기, Time: 17.54 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for (int i = 0; i <= d; i += k) {
+            long maxJ = (long)Math.sqrt((long) d * d - (long) i * i);
+            answer += (maxJ / k) + 1;
+        }
+        
+        return answer;
+    }
+}
