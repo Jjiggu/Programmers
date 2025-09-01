afc01cda2f6451881376a96880bd5d5b41f01937
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-01 18:50:35 +0900

    [level 3] Title: 단속카메라, Time: 10.07 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,23 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, new Comparator<int[]>(){
-            @Override
-            public int compare(int[] r1, int[] r2) {
-                return r1[1] - r2[1];
-            }
-        }); 
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]); 
         
         int answer = 0;
         int pos = Integer.MIN_VALUE;
         
-        for (int[] route : routes) {
-            int start = route[0];
-            int end = route[1];
-            
-            if (start > pos || pos > end) {
+        for (int[] route : routes) {    
+            if (pos < route[0]) {
+                pos = route[1];
                 answer++;
-                pos = end;
             }
         }
         
