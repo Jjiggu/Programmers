a3ebcc12bb77001a9f36254c58a3e876cf0a2307
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-08 15:59:57 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 872.63 ms, Memory: 519 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,66 +1,74 @@
 import java.util.*;
 
 class Solution {
+    
     class Node {
         int x;
         int y;
         int dir;
         int cost;
         
-        Node(int x, int y, int cost, int dir) {
+        Node(int x, int y, int dir, int cost) {
             this.x = x;
             this.y = y;
-            this.cost = cost;
             this.dir = dir;
+            this.cost = cost;
         }
     }
-    static int[] dx = {-1, 1, 0, 0}; 
+    
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-
+    
     public int solution(int[][] board) {
         int n = board.length;
-        int[][][] cost = new int[n][n][4];
+        return bfs(n, board);
+    }
+    
+    private int bfs(int n, int[][] board) {
         
-        for (int[][] layer : cost) {
-            for (int[] row : layer) {
-                Arrays.fill(row, Integer.MAX_VALUE);
+        int[][][] minCost = new int[n][n][4];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(minCost[i][j], Integer.MAX_VALUE);
             }
         }
         
-        return bfs(board, cost, n);
-    }
-    
-    
-    private int bfs(int[][] board, int[][][] cost, int n) {
         Deque<Node> q = new ArrayDeque<>();
+        q.offer(new Node(0, 0, -1, 0));
         
-        if (board[0][1] == 0) {
-            q.add(new Node(0, 1, 100, 3));
-            cost[0][1][0] = 100;
-        }
-        
-        if (board[1][0] == 0) {
-            q.add(new Node(1, 0, 100, 1));
-            cost[1][0][3] = 100;
-        }
+        int answer = Integer.MAX_VALUE;
         
         while (!q.isEmpty()) {
             Node cur = q.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int dir = cur.dir;
+            int cost = cur.cost;
+            
+            if (x == n - 1 && y == n - 1) {
+                answer = Math.min(answer, cost);
+                continue;
+            }
             
             for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n || board[nx][ny] == 1) continue;
-                int newCost = cur.dir == i ? cur.cost + 100 : cur.cost + 600;
-                
-                if (newCost < cost[nx][ny][i]) {
-                    cost[nx][ny][i] = newCost;
-                    q.add(new Node(nx, ny, newCost, i));
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && board[nx][ny] == 0) {
+                    int newCost;
+                    if (dir == -1 || dir == i) {
+                        newCost = cost + 100;
+                    } else {
+                        newCost = cost + 600;
+                    }
+                    
+                    if (minCost[nx][ny][i] >= newCost) {
+                        minCost[nx][ny][i] = newCost;
+                        q.offer(new Node(nx, ny, i, newCost));
+                    }
                 }
             }
         }
-        
-        return Arrays.stream(cost[n - 1][n - 1]).min().getAsInt();
+        return answer;
     }
 }
