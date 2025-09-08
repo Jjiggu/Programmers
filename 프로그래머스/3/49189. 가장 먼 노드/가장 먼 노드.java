3bbeec50c43de05bbb16f8ae8f270422114e09bf
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-08 13:29:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 40.87 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,44 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<List<Integer>> graph = buildGraph(n, edge);
-        int[] dist = bfs(1, n, graph);
         
-        return countMax(dist);
+        List<Integer>[] graph = buildGraph(n, edge);     
+        int[] dist = bfs(n, graph, 1);
+        return countFartestNodes(dist);
     }
     
-    
-    private List<List<Integer>> buildGraph(int n, int[][] edge) {
-        List<List<Integer>> graph = new ArrayList<>();
+    private int[] bfs(int n, List<Integer>[] graph, int start) {
         
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int[] e : edge) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
-        }
-        
-        return graph;
-    }
-    
-    
-    private int[] bfs(int start, int n, List<List<Integer>> graph) {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
         
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(start);
         dist[start] = 0;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int cur = q.poll();
-            
-            for (int next : graph.get(cur)) {
+            for (int next : graph[cur]) {
                 if (dist[next] == -1) {
                     dist[next] = dist[cur] + 1;
-                    q.add(next);
+                    q.offer(next);
                 }
             }
         }
@@ -47,19 +30,30 @@ class Solution {
         return dist;
     }
     
-    
-    private int countMax(int[] dist) {
-        int max = 0;
+    private int countFartestNodes(int[] dist) {
+        int maxDist = 0;
         
         for (int d : dist) {
-            if (d > max) max = d;
+            if (d > maxDist) maxDist = d;
         }
         
         int cnt = 0;
         for (int d : dist) {
-            if (d == max) cnt++;
+            if (d == maxDist) cnt++;
         }
         
         return cnt;
+    }
+    
+    private List<Integer>[] buildGraph(int n, int[][] edge) {
+        List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] e : edge) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+        return graph;
     }
 }
