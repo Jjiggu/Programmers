45e50b471bbdc6e62ce99d79f44f7ba5d07774d8
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-08 14:06:02 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 32.52 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,18 +1,27 @@
+import java.util.*;
+
 class Solution {
     public long solution(int[] sequence) {
-        long maxSum = Integer.MIN_VALUE;
-        long evenSum = 0, oddSum = 0;
+        int n = sequence.length;
+        long[] pulse1 = new long[n];
+        long[] pulse2 = new long[n];
         
-        for (int i = 0; i < sequence.length; i++) {
-            int pulseEven = (i % 2 == 0) ? -1 : 1;
-            int pulseOdd = (i % 2 == 0) ? 1 : -1;
-            
-            evenSum = Math.max(sequence[i] * pulseEven, evenSum + sequence[i] * pulseEven);
-            oddSum = Math.max(sequence[i] * pulseOdd, oddSum + sequence[i] * pulseOdd);
-            
-            maxSum = Math.max(maxSum, Math.max(evenSum, oddSum));
+        pulse1[0] = sequence[0];
+        pulse2[0] = -sequence[0];
+        
+        long answer = Math.max(pulse1[0], pulse2[0]);
+        
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) {
+                pulse1[i] = Math.max(sequence[i], pulse1[i - 1] + sequence[i]); 
+                pulse2[i] = Math.max(-sequence[i], pulse2[i - 1] - sequence[i]); 
+            } else {
+                pulse1[i] = Math.max(-sequence[i], pulse1[i - 1] - sequence[i]); 
+                pulse2[i] = Math.max(sequence[i], pulse2[i - 1] + sequence[i]); 
+            }
+            answer = Math.max(answer, Math.max(pulse1[i], pulse2[i]));
         }
         
-        return maxSum;
+        return answer;
     }
 }
