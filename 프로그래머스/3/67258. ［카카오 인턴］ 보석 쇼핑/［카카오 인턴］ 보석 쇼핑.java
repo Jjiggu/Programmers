0261ea7ffd8670dc74ff52cf91a83344b48bb01e
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-08 12:32:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 71.12 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -3,35 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
         int gemTypes = new HashSet<>(Arrays.asList(gems)).size();
-        int left = 0, right = 0;
-        int minLen = Integer.MAX_VALUE;
+        int left = 0;
+        int length = Integer.MAX_VALUE;
         int answerLeft = 0, answerRight = 0;
-        
         Map<String, Integer> gemList = new HashMap<>();
         
-        
-        while (true) {
-            if (gemList.size() < gemTypes) {
-                if (right == gems.length) break;
-                
-                gemList.put(gems[right], gemList.getOrDefault(gems[right], 0) + 1);
-                right++;
-            } else {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    answerLeft = left;
-                    answerRight = right;
-                }
-                
+        for (int i = 0; i < gems.length; i++) {
+            gemList.put(gems[i], gemList.getOrDefault(gems[i], 0) + 1);
+            
+            while (gemList.get(gems[left]) > 1) {
                 gemList.put(gems[left], gemList.get(gems[left]) - 1);
-                
-                if (gemList.get(gems[left]) == 0) {
-                    gemList.remove(gems[left]);
-                }
                 left++;
+            }
+            
+            if (gemList.size() == gemTypes && (i - left) < length) {
+                length = i - left;
+                answerLeft = left + 1;
+                answerRight = i + 1;
             }
         }
         
-        return new int[] {answerLeft + 1, answerRight};
+        return new int[]{answerLeft, answerRight};  
     }
 }
