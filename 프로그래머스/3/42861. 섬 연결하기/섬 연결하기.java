b2d20f7aa6e4d5974b0519abf7ab38ce31c8ae9d
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-08 14:30:17 +0900

    [level 3] Title: 섬 연결하기, Time: 0.79 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,35 +1,42 @@
 import java.util.*;
 
 class Solution {    
-    public static void union(int[] parent, int a, int b) {
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+        
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        
+        int answer = 0;
+        int cnt = 0;
+        
+        for (int[] edge : costs) {
+            int a = edge[0];
+            int b = edge[1];
+            int cost = edge[2];
+            
+            if (find(parent, a) != find(parent, b)) {
+                union(parent, a, b);
+                answer += cost;
+                cnt++;
+                if (cnt == n - 1) break;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private int find(int[] parent, int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent, parent[x]);
+    }
+    
+    private void union(int[] parent, int a, int b) {
         a = find(parent, a);
         b = find(parent, b);
         
         if (a != b) {
             parent[b] = a;
         }
-    }
-    
-    public static int find(int[] parent, int a) {
-        if (parent[a] == a) return a;
-        else return parent[a] = find(parent, parent[a]);
-    }
-    
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-        int[] parent = new int[n];
-        
-        for (int i = 0; i < n; i++) parent[i] = i;
-        
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        
-        for (int i = 0; i < costs.length; i++) {
-            if (find(parent, costs[i][0]) != find(parent, costs[i][1])) {
-                union(parent, costs[i][0], costs[i][1]);
-                answer += costs[i][2];
-            }
-        }
-        
-        return answer;
     }
 }
