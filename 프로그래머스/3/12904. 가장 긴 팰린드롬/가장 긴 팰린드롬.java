ea84afc88d53cbc21a3be3a0e470eb5b6b9ececd
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-09 23:59:50 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 19.41 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,8 +1,9 @@
 class Solution{
     public int solution(String s) {
+        int n = s.length();
         int answer = 1;
         
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < n; i++) {
             answer = Math.max(answer, expand(i - 1, i + 1, s));
             answer = Math.max(answer, expand(i, i + 1, s));
         }
@@ -10,7 +11,8 @@ class Solution{
         return answer;
     }
     
-    public int expand(int left, int right, String s) {
+    private int expand(int left, int right, String s) {
+        
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
@@ -18,4 +20,4 @@ class Solution{
         
         return right - left - 1;
     }
-}
+} 
