d3705a97fe8aaa5761408dec7cb3af8951797977
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-09 23:25:01 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.34 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,46 +1,42 @@
 import java.util.*;
 
-class Job {
-    int reqTime;
-    int workTime;
-    
-    public Job(int reqTime, int workTime) {
-        this.reqTime = reqTime;
-        this.workTime = workTime;
-    }
-}
-
 class Solution {
+    
+    class Job {
+        int requestTime;
+        int duration;
+        
+        public Job(int requestTime, int duration) {
+            this.requestTime = requestTime;
+            this.duration = duration;
+        }
+    }
+    
     public int solution(int[][] jobs) {
-        int[] answer = new int[jobs.length];
+        PriorityQueue<Job> pq = new PriorityQueue<>((o1, o2) -> o1.duration - o2.duration);
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
         
-        PriorityQueue<Job> pq = new PriorityQueue<>((o1, o2) -> o1.workTime - o2.workTime);
-        
-        
+        int total = 0;
         int time = 0;
-        int totalTime = 0;
-        int cnt = 0;
         int idx = 0;
+        int count = jobs.length;
         
-        while (cnt < jobs.length) {
-            while (idx < jobs.length && jobs[idx][0] <= time) {
+        while (idx < count || !pq.isEmpty()) {
+            while (idx < count && jobs[idx][0] <= time) {
                 pq.offer(new Job(jobs[idx][0], jobs[idx][1]));
                 idx++;
             }
             
-            if (pq.isEmpty()) {
+            if (!pq.isEmpty()) {
+                Job cur = pq.poll();
+                time += cur.duration;
+                total += (time - cur.requestTime);
+            } else {
                 time = jobs[idx][0];
-                continue;
             }
-            
-            Job now = pq.poll();
-            time += now.workTime;
-            totalTime += (time - now.reqTime);
-            cnt++;
         }
-
         
-        return totalTime / jobs.length;
+        
+        return total / count;
     }
 }
