903bc2c7cfdab093f03bad730642f3074f13e2d8
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-09 19:50:55 +0900

    [level 3] Title: 최고의 집합, Time: 0.32 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int n, int s) {
-        
-        if (n > s) return new int[] {-1};
+        if (n > s) return new int[]{-1};
         
         int fillNum = s / n;
-        int sumCnt = s % n;
+        int modNum = s % n;
         
         int[] answer = new int[n];
         Arrays.fill(answer, fillNum);
         
         for (int i = answer.length - 1; i >= 0; i--) {
-            if (sumCnt == 0) break;
-            
+            if (modNum == 0) break;
             answer[i] += 1;
-            sumCnt--;
+            modNum--;
         }
         
         return answer;
