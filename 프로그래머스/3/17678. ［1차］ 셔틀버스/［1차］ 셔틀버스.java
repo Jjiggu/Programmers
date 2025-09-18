6386b5ff1895dfa2b0c10193f48e9d94b9fd2778
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-18 17:18:37 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 5.27 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,56 +1,50 @@
 import java.util.*;
 
 class Solution {
-    static final int START_TIME = 540;
-    
     public String solution(int n, int t, int m, String[] timetable) {
+        int depart = 540;
         int answer = 0;
+        
         int[] times = new int[timetable.length];
         
         for (int i = 0; i < timetable.length; i++) {
-            times[i] = stringToTime(timetable[i]);
+            times[i] = timeToInt(timetable[i]);
         }
         
         Arrays.sort(times);
         
-        int depart = START_TIME;
         int idx = 0;
         
         for (int i = 0; i < n; i++) {
             int boardingCnt = 0;
             
             while (boardingCnt < m && idx < times.length && times[idx] <= depart) {
+                // 탑승하는 사람 수 계산
                 boardingCnt++;
                 idx++;
-            }
+            }      
             
             if (i == n - 1) {
                 if (boardingCnt < m) {
-                    answer = depart;  
+                    answer = depart;
                 } else {
                     answer = times[idx - 1] - 1;
                 }
-            } 
+            }
             
             depart += t;
         }
         
-        return timeTostring(answer);
-    }   
-    
-    private int stringToTime(String time) {
-        String[] arr = time.split(":");
-
-        int hour = Integer.parseInt(arr[0]) * 60;
-        int min = Integer.parseInt(arr[1]);
-        
-        return hour + min;
+        return IntToTime(answer);
     }
     
-    private String timeTostring(int time) {
-        int hour = time / 60;
-        int min = time % 60;;
+    private String IntToTime(int time) {
+        return String.format("%02d:%02d", time / 60, time % 60);
+    }
+    
+    private int timeToInt(String time) {
+        String[] t = time.split(":");
         
-        return String.format("%02d:%02d", hour, min);
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
     }
 }
