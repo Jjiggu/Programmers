dced984e1b4ce1c2d1d996bf74d64bebf4ec45c5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-09-18 16:38:50 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 72.78 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        
-        int[] answer = new int[enroll.length];
         Map<String, String> group = new HashMap<>();
         Map<String, Integer> profit = new HashMap<>();
         
+        // 레퍼럴, 수익 초기화 
         for (int i = 0; i < enroll.length; i++) {
             group.put(enroll[i], referral[i]);
-            profit.put(enroll[i], profit.getOrDefault(enroll[i], 0));
+            profit.put(enroll[i], 0);
         }
         
-        
-        for (int i = 0; i < amount.length; i++) {
-            int price = amount[i] * 100;
+        // 수익 계산
+        for (int i = 0; i < seller.length; i++) {
             String sellerName = seller[i];
+            int price = amount[i] * 100;
             
-            while (!sellerName.equals("-") && price > 0) {
-                int refProfit = price / 10;
-                int myProfit = price - refProfit;   
+            while (!sellerName.equals("-") && price > 0) { // 추천인 없을때까지 
+                int refIncome = price / 10;
+                int income = price - refIncome;
                 
-                profit.put(sellerName, profit.get(sellerName) + myProfit);
-                
-                sellerName = group.get(sellerName);
-                price = refProfit;  
+                profit.put(sellerName, profit.get(sellerName) + income);
+    
+                sellerName = group.get(sellerName);;
+                price = refIncome;
             }
         }
         
+        int[] answer = new int[enroll.length];
         for (int i = 0; i < enroll.length; i++) {
             answer[i] = profit.get(enroll[i]);
         }
