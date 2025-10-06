633153a76142d41d611dc972a9f39805a8c7fcf5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-06 22:57:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.69 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 80.3 MB, 시간: 15.94 ms
+메모리: 84.2 MB, 시간: 2.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 02일 14:14:21
+2025년 10월 06일 22:57:20
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,23 +1,14 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
+        String temp;
         
-        for(int i = 0; i < n; i++) {
-            // String binary1 = Integer.toBinaryString(arr1[i]);
-            // String binary2 = Integer.toBinaryString(arr2[i]);
-            String binary1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(' ', '0');
-            String binary2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(' ', '0');
-            StringBuilder sb = new StringBuilder();
-            
-            for (int k = 0; k < binary1.length(); k++) {
-                if (binary1.charAt(k) == '1' || binary2.charAt(k) == '1') {
-                    sb.append("#");
-                } else {
-                    sb.append(" ");
-                }
-            }
-            
-            answer[i] = sb.toString();
+        for (int i = 0; i < arr1.length; i++) {
+            temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            temp = temp.substring(temp.length() - n);
+            temp = temp.replaceAll("1", "#");
+            temp = temp.replaceAll("0", " ");
+            answer[i] = temp;
         }
         
         return answer;
