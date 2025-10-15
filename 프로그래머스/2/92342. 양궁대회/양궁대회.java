b4aaa6cf222e478c3184d960868debc36e1a27f5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-15 13:39:27 +0900

    [level 2] Title: 양궁대회, Time: 0.90 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     
+    static int maxScore = 0;
     static int[] answer = {-1};
-    static int maxDiff = 0;;
     
     public int[] solution(int n, int[] info) {
-        
+    
         dfs(0, n, new int[11], info);
         
         return answer;
@@ -14,23 +14,17 @@ class Solution {
     
     private void dfs(int k, int remain, int[] ryan, int[] info) {
         if (k == 11) {
-            if (remain > 0) ryan[10] += remain;
+            if (remain > 0) ryan[10] += remain;  // 화살이 남은 경우
             
-            int ryanScore = 0;
-            int apeachScore = 0;
-            for (int i = 0 ; i <11; i++) {
-                if (ryan[i] == 0 && info[i] == 0) continue;
-                if (ryan[i] > info[i]) ryanScore += (10 - i);
-                else apeachScore += (10 - i);
-            }
+            int[] scoreArr = calcScore(ryan, info);
+            int diff = scoreArr[0] - scoreArr[1];
             
-            int diff = ryanScore - apeachScore;
-            if (diff > 0) {
-                if (diff > maxDiff) {
-                    maxDiff = diff;
-                    answer = Arrays.copyOf(ryan, 11);   
-                } else if (diff == maxDiff) {
-                    for (int i = 10; i >= 0; i--) {
+            if (diff > 0) {  // Ryan이 이긴경우
+                if (diff > maxScore) {  // 기존 점수 차이보다 큰 경우 
+                    maxScore = diff;
+                    answer = Arrays.copyOf(ryan, 11);
+                } else if (diff == maxScore) { // 점수 차이 같은 경우
+                    for (int i = 10; i >= 0; i--) {  // 낮은거 개수 비교
                         if (ryan[i] > answer[i]) {
                             answer = Arrays.copyOf(ryan, 11);
                             break;
@@ -40,14 +34,29 @@ class Solution {
             } 
             
             if (remain > 0) ryan[10] -= remain;
+            
             return;
         }
         
-        if (remain > info[k]) {
+        if (remain > info[k]) {  // 화살 쏘는 경우
             ryan[k] = info[k] + 1;
             dfs(k + 1, remain - ryan[k], ryan, info);
-            ryan[k] = 0;
+            ryan[k] = 0; 
+        } 
+        
+        dfs(k + 1, remain, ryan, info);  // 화살 쏘지 않는 경우
+    }
+    
+    private int[] calcScore(int[] ryan, int[] info) {
+        int ryanCalc = 0;
+        int apeachCalc = 0;
+        
+        for (int i = 0; i < 11; i++) {
+            if (ryan[i] == 0 && info[i] == 0) continue;
+            if (ryan[i] > info[i]) ryanCalc += (10 - i);
+            else apeachCalc += (10 - i);
         }
-        dfs(k + 1, remain, ryan, info);
+        
+        return new int[]{ryanCalc, apeachCalc};
     }
 }
