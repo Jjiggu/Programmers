2005addb16bf376869ea3587b0df5df3770237e7
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-15 10:20:04 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.30 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -2,23 +2,24 @@ class Solution {
     private static final int MOD = 1_000_000_007;
 
     public int solution(int n) {
-        if (n % 2 == 1) return 0;      
-
-        long[] dp  = new long[n + 1];
-        long[] acc = new long[n + 1];  
-
+        if (n % 2 == 1) return 0;     
+        
+        long[] dp = new long[n + 1];
+        long[] edge = new long[n + 1];
+        
         dp[0] = 1;
-        acc[0] = 1;
+        edge[0] = 1;
+        
         if (n >= 2) {
             dp[2] = 3;
-            acc[2] = (acc[0] + dp[2]) % MOD;
+            edge[2] = (edge[0] + dp[2]) % MOD;
         }
-
+        
         for (int i = 4; i <= n; i += 2) {
-            dp[i]  = (dp[i - 2] * 3 + 2 * acc[i - 4]) % MOD;
-            acc[i] = (acc[i - 2] + dp[i]) % MOD;
+            dp[i] = (dp[i - 2] * 3 + 2 * edge[i - 4]) % MOD;
+            edge[i] = (edge[i - 2] + dp[i]) % MOD;
         }
-
-        return (int) (dp[n] % MOD);
+        
+        return (int)dp[n];
     }
 }
