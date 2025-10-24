b9e228354fc7280d894fa06c37cced5ac08a42a8
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-24 09:52:45 +0900

    [level 2] Title: 지게차와 크레인, Time: 23.47 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -5,89 +5,92 @@ class Solution {
     static int[] dy = {0, 0, -1, 1};
     
     public int solution(String[] storage, String[] requests) {
-        int n = storage.length;
-        int m = storage[0].length();
-        int[][] map = setOutline(n, m);
+        int[][] storageMap = fillWithZero(storage);
         
         for (String request : requests) {
-            char c = request.charAt(0);
-            if (request.length() == 1) {
-                Deque<int[]> q = new ArrayDeque<>();
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < m; j++) {
-                        if (storage[i].charAt(j) == c && bfs(i + 1, j + 1, c, map)) {
-                            q.add(new int[]{i + 1, j + 1});
-                        }
-                    }
-                }
-                while(!q.isEmpty()) {
-                    int[] cur = q.poll();
-                    map[cur[0]][cur[1]] = -1;
-                }
+            char targetChar = request.charAt(0);
+            int target = (int) targetChar;
+            
+            if (request.length() == 2) {
+                crane(storageMap, target);
             } else {
-                map = removeContainer(c, map, storage);
+                forkLift(storageMap, target);
             }
         }
-        return findAnswer(map);
+        
+        return countRemaining(storageMap);
     }
     
-    public boolean bfs(int startX, int startY, char c, int[][] map) {
-        Deque<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        q.add(new int[]{startX, startY});
-        visited[startX][startY] = true;
+    private void crane(int[][] storageMap, int target) {
+        int n = storageMap.length;
+        int m = storageMap[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (storageMap[i][j] == target) {
+                    storageMap[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    private void forkLift(int[][] storageMap, int target) {
+        int n = storageMap.length;
+        int m = storageMap[0].length;
         
-        while (!q.isEmpty()) {
+        boolean[][] vis = new boolean[n][m];
+        Deque<int[]> q = new ArrayDeque<>();
+        
+        q.add(new int[]{0, 0});
+        vis[0][0] = true;
+        
+        while(!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-            if (map[x][y] == -2) return true;
             
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (visited[nx][ny]) continue;
-                if (map[nx][ny] == 0) continue;
-                q.add(new int[]{nx, ny});
-                visited[nx][ny] = true;
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny]) continue;
+                
+                if (storageMap[nx][ny] == target) {
+                    storageMap[nx][ny] = 0;
+                    vis[nx][ny] = true;
+                } else if (storageMap[nx][ny] == 0) {
+                    vis[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
+                }
             }
         }
         
-        return false;
+        return;
     }
     
-    public int[][] removeContainer(char c, int[][] map, String[] storage) {
-        for (int i = 1; i < map.length - 1; i++) {
-            for (int j = 1; j < map[0].length - 1; j++) {
-                if (storage[i - 1].charAt(j - 1) == c) map[i][j] = -1;
-            }
-        }
-        return map;
-    }
-    
-    public int findAnswer(int[][] map) {
-        int cnt = 0;
-        
-        for (int i = 1; i < map.length - 1; i++) {
-            for (int j = 1; j < map[0].length - 1; j++) {
-                if (map[i][j] == 0) cnt++;
-            }
-        }
-        
-        return cnt;
-    }
-    
-    public int[][] setOutline(int n, int m) {
+    private int[][] fillWithZero(String[] storage) {
+        int n = storage.length;
+        int m = storage[0].length();
         int[][] map = new int[n + 2][m + 2];
         
-        for (int i = 0; i < n + 2; i++) {
-            for (int j = 0; j < m + 2; j++) {
-                if (i == 0) map[i][j] = -2;
-                if (j == 0) map[i][j] = -2;
-                if (i == n + 1) map[i][j] = -2;
-                if (j == m + 1) map[i][j] = -2;
+        for (int i = 0; i < n; i++) {
+            String row = storage[i];
+            for (int j = 0; j < m; j++) {
+                map[i + 1][j + 1] = row.charAt(j); 
             }
         }
+        
         return map;
+    }
+    
+    private int countRemaining(int[][] storageMap) {
+        int totalRows = storageMap.length;
+        int totalCols = storageMap[0].length;
+        int cnt = 0;
+        for (int i = 1; i < totalRows - 1; i++) {
+            for (int j = 1; j < totalCols - 1; j++) {
+                if (storageMap[i][j] != 0) cnt++;
+            }
+        }
+        return cnt;
     }
 }
