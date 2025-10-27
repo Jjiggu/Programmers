46040de51ac1f4bec52479f4e9bfac1003f892a4
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-27 13:14:05 +0900

    [level 2] Title: 비밀 코드 해독, Time: 166.09 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,40 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static int answer = 0;
+    List<int[]> comb = new ArrayList<>();
     
     public int solution(int n, int[][] q, int[] ans) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        int answer = 0;
         
-        dfs(0, 0, arr, n, q, ans);
+        dfs(0, n, new ArrayList<>());
+        
+        for (int[] com : comb) {
+            answer += check(com, q, ans);
+        }
         
         return answer;
     }
     
-    private void dfs(int start, int k, ArrayList<Integer> arr, int n, int[][] q, int[] ans) {
+    private int check(int[] com, int[][] q, int[] ans) {
+        HashSet<Integer> s = new HashSet<>();
+        for (int num : com) s.add(num);
+        
+        for (int i = 0; i < q.length; i++) {
+            int[] inputQ = q[i];
+            int cnt = 0;
+            
+            for (int j = 0; j < inputQ.length; j++) {
+                if (s.contains(inputQ[j])) cnt++;
+                if (cnt > ans[i]) return 0;
+            }    
+            if (cnt != ans[i]) return 0;
+        }
+        
+        return 1;
+    }
+    
+    private void dfs(int k, int n, List<Integer> arr) {
+        
         if (arr.size() == 5) {
-            if (check(arr, q, ans)) answer++;
+            comb.add(arr.stream().mapToInt(Integer::intValue).toArray());
             return;
         }
         
-        int cnt = 0;
-        for (int i = start; i < n; i++) {
+        for (int i = k; i < n; i++) {
             arr.add(i + 1);
-            dfs(i + 1, k + 1, arr, n, q, ans);
+            dfs(i + 1, n, arr);
             arr.remove(arr.size() - 1);
         }
-    }
-    
-    private boolean check(ArrayList<Integer> arr, int[][] q, int[] ans) {
-        for (int i = 0; i < q.length; i++) {
-            int cnt = 0;
-            for (int num : q[i]) {
-                if (arr.contains(num)) cnt++;
-            }
-            
-            if (cnt != ans[i]) return false;
-        }
-        
-        return true;   
     }
 }
