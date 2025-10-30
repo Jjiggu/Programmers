f86b497670531b10cd5e4344e72caa0f0a56c417
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-10-30 16:05:20 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 1.48 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,106 +1,130 @@
 import java.util.*;
 
 class Solution {
-    static class State {
-        int rPos, bPos;
-        int rMask, bMask;
-        int dist;
-        State(int rPos, int bPos, int rMask, int bMask, int dist) {
-            this.rPos = rPos;
-            this.bPos = bPos;
-            this.rMask = rMask;
-            this.bMask = bMask;
-            this.dist = dist;
+    
+    class State {
+        
+        public int rx, ry, bx, by, t; // 빨간색 (x,y), 파란색 (x,y), 턴
+        public boolean[][][] visited; // 방문 배열
+        
+        public State(int rx, int ry, int bx, int by, int t, boolean[][][] visited) {
+            this.rx = rx; this.ry = ry; 
+            this.bx = bx; this.by = by;
+            this.t = t;
+            
+            // 현재 위치 방문 처리
+            this.visited = visited;
+            this.visited[0][rx][ry] = true;
+            this.visited[1][bx][by] = true;
         }
+        
     }
     
+    private int rix, riy, bix, biy; // 시작 위치
+    private int rdx, rdy, bdx, bdy; // 목표 위치
+    private boolean[][][][] mem = new boolean[4][4][4][4]; // 현재 State에 방문한적이 있는지 mem[rx][ry][bx][by]
+    private int[] dx = {-1,1,0,0};
+    private int[] dy = {0,0,-1,1};
+    
     public int solution(int[][] maze) {
-        int n = maze.length, m = maze[0].length;
-        int redStart=0, blueStart=0, redGoal=0, blueGoal=0;
-        boolean[] wall = new boolean[n*m];
+        init(maze); // 시작 위치와 도착 위치 찾기
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int idx = i*m + j;
-                switch (maze[i][j]) {
-                    case 1: redStart = idx; break;
-                    case 2: blueStart = idx; break;
-                    case 3: redGoal  = idx; break;
-                    case 4: blueGoal = idx; break;
-                    case 5: wall[idx] = true; break;
-                }
-            }
-        }
-        
-        int initRmask = 1 << redStart;
-        int initBmask = 1 << blueStart;
-        Queue<State> q = new ArrayDeque<>();
-        Set<Long> seen = new HashSet<>();
-        
-        State init = new State(redStart, blueStart, initRmask, initBmask, 0);
-        q.add(init);
-        seen.add(encode(init));
-        
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = {  0, 0, -1, 1 };
+        Queue<State> q = new LinkedList<>();
+        q.offer(new State(rix, riy, bix, biy, 0, new boolean[2][4][4])); // 초기 State
+        mem[rix][riy][bix][biy] = true; // 초기 State 방문 처리
         
         while (!q.isEmpty()) {
-            State s = q.poll();
-            if (s.rPos == redGoal && s.bPos == blueGoal) {
-                return s.dist;
-            }
+            State cur = q.peek();
+            q.poll();
             
-            // try all 4×4 moves
-            for (int d1 = 0; d1 < 4; d1++) {
-                int nrPos = s.rPos;
-                if (s.rPos != redGoal) {
-                    int rr = s.rPos / m + dr[d1];
-                    int rc = s.rPos % m + dc[d1];
-                    if (rr < 0 || rr >= n || rc < 0 || rc >= m) continue;
-                    int ni = rr*m + rc;
-                    if (wall[ni] || ((s.rMask >> ni) & 1) == 1) continue;
-                    nrPos = ni;
+            if (isGameClear(cur)) return cur.t; // 목표 위치에 전부 도달했으면 현재 State의 턴 반환
+            
+            for (int i=0; i<4; i++) {
+                boolean redClear = isRedClear(cur); // 빨간 수레의 클리어 여부
+                int nrx = cur.rx;
+                int nry = cur.ry;
+                
+                // 아직 클리어하지 못했으면 이동
+                if (!redClear) {
+                    nrx += dx[i];
+                    nry += dy[i];
                 }
                 
-                for (int d2 = 0; d2 < 4; d2++) {
-                    int nbPos = s.bPos;
-                    if (s.bPos != blueGoal) {
-                        int br = s.bPos / m + dr[d2];
-                        int bc = s.bPos % m + dc[d2];
-                        if (br < 0 || br >= n || bc < 0 || bc >= m) continue;
-                        int ni = br*m + bc;
-                        if (wall[ni] || ((s.bMask >> ni) & 1) == 1) continue;
-                        nbPos = ni;
+                if (!isValid(nrx, nry, maze)) continue; // 이동하려는 위치가 퍼즐 밖이거나 벽인 경우
+                if (!redClear && cur.visited[0][nrx][nry]) continue; // 이미 방문한 위치인 경우 (목표 위치 제외) 
+                
+                for (int j=0; j<4; j++) {
+                    boolean blueClear = isBlueClear(cur); // 파란 수레의 클리어 여부
+                    int nbx = cur.bx;
+                    int nby = cur.by;
+                    
+                    if (!blueClear) {
+                        nbx += dx[j];
+                        nby += dy[j];
                     }
                     
-                    // 동시에 같은 칸 이동 금지
-                    if (nrPos == nbPos) continue;
-                    // 자리 바꿔치기 금지
-                    if (nrPos == s.bPos && nbPos == s.rPos) continue;
+                    if (!isValid(nbx, nby, maze)) continue;
+                    if (!blueClear && cur.visited[1][nbx][nby]) continue;
+                    if (isSame(nrx, nry, nbx, nby) || isSwapped(nrx, nry, nbx, nby, cur)) continue; // 두 수레가 같은 위치이거나 서로 자리를 바꾼 경우
+                    if (mem[nrx][nry][nbx][nby]) continue; // 이미 방문한 State인 경우
                     
-                    int nrMask = s.rMask;
-                    if (s.rPos != redGoal) nrMask |= 1 << nrPos;
-                    int nbMask = s.bMask;
-                    if (s.bPos != blueGoal) nbMask |= 1 << nbPos;
-                    
-                    State ns = new State(nrPos, nbPos, nrMask, nbMask, s.dist + 1);
-                    long code = encode(ns);
-                    if (seen.add(code)) {
-                        q.add(ns);
-                    }
+                    boolean[][][] newVisited = copyVisited(cur.visited); // 방문 배열 깊은 복사
+                    mem[nrx][nry][nbx][nby] = true; // 현재 State 방문 처리
+                    q.offer(new State(nrx, nry, nbx, nby, cur.t+1, newVisited));
                 }
             }
+            
         }
         
         return 0;
     }
     
-    private long encode(State s) {
-        // 4 bits for redPos, 4 bits for bluePos, 16 bits each for masks
-        long code =  ((long)s.rPos << 36)
-                   | ((long)s.bPos << 32)
-                   | ((long)s.rMask << 16)
-                   |  (long)s.bMask;
-        return code;
+    public void init(int[][] maze) {
+        for (int i=0; i<maze.length; i++) {
+            for (int j=0; j<maze[i].length; j++) {
+                switch (maze[i][j]) {
+                    case 1 : rix = i; riy = j; break;
+                    case 2 : bix = i; biy = j; break;
+                    case 3 : rdx = i; rdy = j; break;
+                    case 4 : bdx = i; bdy = j; break;
+                }
+            }
+        }
+    }
+    
+    public boolean[][][] copyVisited(boolean[][][] a) {
+        boolean[][][] b = new boolean[a.length][a[0].length][a[0][0].length];
+        for (int i=0; i<a.length; i++) {
+            for (int j=0; j<a[i].length; j++) {
+                for (int k=0; k<a[i][j].length; k++) {
+                    b[i][j][k] = a[i][j][k];
+                }
+            }
+        }
+        return b;
+    }
+    
+    public boolean isValid(int x, int y, int[][] maze) {
+        return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] != 5;
+    }
+    
+    public boolean isSame(int rx, int ry, int bx, int by) {
+        return rx == bx && ry == by;
+    }
+    
+    public boolean isSwapped(int nrx, int nry, int nbx, int nby, State bef) {
+        return nrx == bef.bx && nry == bef.by && nbx == bef.rx && nby == bef.ry;
+    }
+    
+    public boolean isRedClear(State st) {
+        return st.rx == rdx && st.ry == rdy;
+    }
+    
+    public boolean isBlueClear(State st) {
+        return st.bx == bdx && st.by == bdy;
+    }
+    
+    public boolean isGameClear(State st) {
+        return st.rx == rdx && st.ry == rdy && st.bx == bdx && st.by == bdy;
     }
 }
