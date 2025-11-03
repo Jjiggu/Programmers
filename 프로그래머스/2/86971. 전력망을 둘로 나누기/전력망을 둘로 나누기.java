faba14e4df4f8c7d9e629090980b137cb378eb76
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-03 17:24:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,6 +5,37 @@ class Solution {
     List<Integer>[] graph;
     
     public int solution(int n, int[][] wires) {
+        int answer = n;
+        initGraph(n, wires);
+        
+        for (int[] wire : wires) {
+            int blockU = wire[0];
+            int blockV = wire[1];
+            
+            boolean[] visited = new boolean[n + 1];
+            
+            int comp = dfs(blockU, -1, blockU, blockV, visited, graph);
+            
+            int diff = Math.abs(2 * comp - n);
+            answer = Math.min(answer, diff);
+        }
+        
+        return answer;
+    }
+    
+    private int dfs(int cur, int parent, int blockU, int blockV, boolean[] visited, List<Integer>[] graph) {
+        visited[cur] = true;
+        int cnt = 1;
+        
+        for (int next : graph[cur]) {
+            if ((cur == blockU && next == blockV) || (cur == blockV && next == blockU)) continue;
+            if (!visited[next]) cnt += dfs(next, cur, blockU, blockV, visited, graph);
+        }
+        
+        return cnt;
+    }
+    
+    private void initGraph(int n, int[][] wires) {
         graph = new ArrayList[n + 1];
         
         for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
@@ -17,31 +48,6 @@ class Solution {
             graph[v].add(u);
         }
         
-        int answer = n;
-        
-        for (int[] wire : wires) {
-            int blockU = wire[0];
-            int blockV = wire[1];
-            
-            boolean[] visited = new boolean[n + 1];
-            
-            int comp = dfs(blockU, -1, blockU, blockV, visited, graph);
-            
-            int diff = Math.abs(n - 2 * comp);
-            answer = Math.min(answer, diff);
-        }
-        
-        return answer;
-    }
-    
-    private int dfs(int cur, int parent, int blockU, int blockV, boolean[] visited, List<Integer>[] g) {
-        visited[cur] = true;
-        int cnt = 1;
-        
-        for (int next : graph[cur]) {
-            if ((cur == blockU && next == blockV) || (cur == blockV && next == blockU)) continue;
-            if (!visited[next]) cnt += dfs(next, cur, blockU, blockV, visited, g);
-        }
-        return cnt;
-    }
+        return;
+    } 
 }
