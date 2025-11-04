979a352c0bd5f0e636bba5a94d667ae6ec2c578a
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-04 17:03:12 +0900

    [level 2] Title: 주식가격, Time: 18.85 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
+        int n = prices.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
         
-        // prices 돌면서 현재 인덱스의 숫자보다 작은 숫자 만나면 그때 인덱스 - 기준 숫자 인덱스
-        for (int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < n; i++) {
             int cnt = 0;
             
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[i] > prices[j]) {
+            for (int j = i + 1; j < n; j++) {        
+                if (prices[j] < prices[i]) {
                     answer[i] = j - i;
                     break;
                 } else {
-                    answer[i] = prices.length - (i + 1);
+                    answer[i] = n - (i + 1);
                 }
             }
         }
