9c9dff0b0a0e7f92fa7c83011da54f4dff174089
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-10 00:31:35 +0900

    [level 3] Title: 사라지는 발판, Time: 9.98 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -2,14 +2,18 @@ class Solution {
     
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    int n, m;
     
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        int[] result = dfs(board, aloc[0], aloc[1], bloc[0], bloc[1]);
+        n = board.length;
+        m = board[0].length;
+        
+        int[] result = dfs(aloc[0], aloc[1], bloc[0], bloc[1], board);
         
         return result[1];
     }
     
-    private int[] dfs(int[][] board, int x, int y, int ox, int oy) {
+    private int[] dfs(int x, int y, int ox, int oy, int[][] board) {
         if (board[x][y] == 0) return new int[]{0, 0};
         
         boolean canWin = false;
@@ -19,16 +23,16 @@ class Solution {
         
         board[x][y] = 0;
         
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            if (nx < 0 || ny < 0 || nx >= board.length || ny >= board[0].length) continue;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
             if (board[nx][ny] == 0) continue;
             
             moved = true;
             
-            int[] res = dfs(board, ox, oy, nx, ny);
+            int[] res = dfs(ox, oy, nx, ny, board);
             
             if (res[0] == 0) {
                 canWin = true;
@@ -39,9 +43,9 @@ class Solution {
         }
         
         board[x][y] = 1;
-        
+            
         if (!moved) return new int[]{0, 0};
-        
+
         if (canWin) return new int[]{1, minWin + 1};
         else return new int[]{0, maxLose + 1};
     }
