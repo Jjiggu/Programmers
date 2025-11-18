8395c9d5e2bd0ad7433364e75ca7132ee01a9c45
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-18 13:52:21 +0900

    [level 3] Title: GPS, Time: 60.04 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.java
+++ 프로그래머스/3/1837. GPS/GPS.java
@@ -1,42 +1,55 @@
 import java.util.*;
 
 class Solution {
-        public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
-        // 1. 그래프 생성
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
-        for (int[] e : edge_list) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
+    
+    final int INF = 1_000_000_000;
+    List<Integer>[] graph;
+    
+    public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
+        initGraph(n, edge_list);
+        
+        int[][] dp = new int[k][n + 1];
+        for (int i = 0; i <k; i++) {
+            Arrays.fill(dp[i], INF);
         }
-        // 2. DP 초기화
-        final int INF = 1_000_000_000;
-        int[][] dp = new int[k][n+1];
-        for (int[] row : dp) Arrays.fill(row, INF);
+        
         dp[0][gps_log[0]] = 0;
-
-        // 3. DP 전이
-        for (int t = 0; t < k-1; t++) {
-            // 이동·머무르기 전이
-            for (int u = 1; u <= n; u++) {
-                if (dp[t][u] == INF) continue;
-                // 머무르기
-                dp[t+1][u] = Math.min(dp[t+1][u], dp[t][u]);
-                // 인접 이동
-                for (int v : adj.get(u)) {
-                    dp[t+1][v] = Math.min(dp[t+1][v], dp[t][u]);
+        
+        for (int i = 0; i < k - 1; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dp[i][j] == INF) continue;
+                
+                dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j]);
+                
+                for (int v : graph[j]) {
+                    dp[i + 1][v] = Math.min(dp[i + 1][v], dp[i][j]);
                 }
             }
-            // 로그 수정 비용 반영
+            
             for (int v = 1; v <= n; v++) {
-                if (v != gps_log[t+1]) {
-                    dp[t+1][v]++;
+                if (v != gps_log[i + 1]) {
+                    dp[i + 1][v]++;
                 }
             }
         }
-
-        // 4. 결과 반환
-        int ans = dp[k-1][ gps_log[k-1] ];
-        return ans >= INF ? -1 : ans;
+        
+        int answer = dp[k - 1][gps_log[k - 1]];
+        
+        return answer >= INF ? -1 : answer;
+    }
+    
+    private void initGraph(int n, int[][] edge_list) {
+        graph = new ArrayList[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] edge : edge_list) {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
     }
 }
