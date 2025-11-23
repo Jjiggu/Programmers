5601ef21127551b169ebb0dcdceedf7eb97418b6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-23 18:17:44 +0900

    [level 2] Title: 삼각 달팽이, Time: 11.74 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         int[][] a = new int[n][n];
-        int x = -1, y = 0;        // 시작 전에 한 칸 위에서 시작한다고 생각
-        int num = 1;              // 채울 숫자
-        int dir = 0;              // 0: down, 1: right, 2: up-left
-
+        int x = -1, y = 0;
+        int num = 1;
+        int dir = 0;
+        
         for (int len = n; len >= 1; len--) {
             for (int k = 0; k < len; k++) {
-                if (dir == 0) {       // down
+                if (dir == 0) {
                     x++;
-                } else if (dir == 1) {// right
+                } else if (dir == 1) {
                     y++;
-                } else {              // up-left
+                } else {
                     x--;
                     y--;
                 }
@@ -21,15 +21,17 @@ class Solution {
             }
             dir = (dir + 1) % 3;
         }
-
+        
         int total = n * (n + 1) / 2;
-        int[] res = new int[total];
         int idx = 0;
+        int[] answer = new int[total];
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                res[idx++] = a[i][j];
+                answer[idx++] = a[i][j];
             }
         }
-        return res;
+        
+        return answer;
     }
 }
