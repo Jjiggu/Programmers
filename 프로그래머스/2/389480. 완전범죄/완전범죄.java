3d0b54069f075e86a57bd85e57e5fa089fcd18ab
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-26 09:18:18 +0900

    [level 2] Title: 완전범죄, Time: 0.90 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -2,23 +2,24 @@ class Solution {
     static int answer = Integer.MAX_VALUE;
     
     public int solution(int[][] info, int n, int m) {
-        
         boolean[][][] visited = new boolean[info.length][n][m];
+        
         dfs(0, info, n, m, 0, 0, visited);
         
-        return (answer == Integer.MAX_VALUE) ? -1 : answer;
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
     
     private void dfs(int k, int[][] info, int n, int m, int a, int b, boolean[][][] visited) {
         if (a >= n || b >= m) return;
         
         if (k == info.length) {
-            if (a < answer) answer = a;
+            answer = Math.min(answer, a);
             return;
         }
         
         if (a >= answer) return;
         if (visited[k][a][b]) return;
+        
         visited[k][a][b] = true;
         
         dfs(k + 1, info, n, m, a + info[k][0], b, visited);
