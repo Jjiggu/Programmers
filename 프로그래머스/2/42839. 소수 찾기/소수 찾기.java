5e7da79ff62c28dc08fda878be5217d48d208949
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-28 13:42:17 +0900

    [level 2] Title: 소수 찾기, Time: 8.92 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    
+    Set<Integer> set = new HashSet<>();
+    
+    public int solution(String numbers) {
+        int[] num = convertToInt(numbers);
+        
+        dfs(0, num, new boolean[numbers.length()], new StringBuilder());
+        
+        // System.out.println(set);
+        
+        return set.size();
+    }
+    
+    private void dfs(int k, int[] num, boolean[] isUse, StringBuilder sb) {
+        if (k == num.length) {
+            if (isPrime(sb.toString())) set.add(Integer.parseInt(sb.toString()));
+            return;
+        }
+        
+        if (sb.length() > 0 && isPrime(sb.toString())) set.add(Integer.parseInt(sb.toString()));
+        
+        for (int i = 0; i < num.length; i++) {
+            if (isUse[i]) continue;
+                
+            sb.append(num[i]);
+            isUse[i] = true;
+            dfs(k + 1, num, isUse, sb);
+            isUse[i] = false;
+            sb.delete(sb.length() - 1, sb.length());
+        }
+    }
+    
+    private boolean isPrime(String number) {
+        int n = Integer.parseInt(number);
+
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    private int[] convertToInt(String numbers) {
+        int[] number = new int[numbers.length()];
+        int idx = 0;
+        
+        for (String c : numbers.split("")) {
+            number[idx++] = Integer.parseInt(c);
+        }
+        
+        return number;
+    }
+}
