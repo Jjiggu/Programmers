9bec4c58fdd5d9f85ba004dcf599b96c254c3a4f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-11-28 18:47:27 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.46 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -2,18 +2,15 @@ class Solution {
     private static final int MOD = 1_000_000_007;
 
     public int solution(int n) {    
+        int[] dp = new int[n + 1];
         
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
-        long[] dp = new long[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-
+        
         for (int i = 3; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
-
-        return (int) dp[n];
+        
+        return dp[n];
     }
 }
