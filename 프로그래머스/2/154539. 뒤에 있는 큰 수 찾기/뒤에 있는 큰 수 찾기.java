2ab715382f5d75f183aedc5689425c5e8026f993
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-01 17:00:34 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 68.61 ms, Memory: 173 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int n = numbers.length;
+        int[] answer = new int[n];
+        Arrays.fill(answer, -1);
+        
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int cur = numbers[i];
+            
+            while (!stack.isEmpty() && stack.peek() <= cur) {
+                stack.pop();
+            }
+            
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek();
+            } else {
+                answer[i] = -1;
+            }
+            
+            stack.push(cur);
+        }
+        
+        return answer;
+    }
+}
