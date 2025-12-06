33f0746fe00ee8328124558edf488949327e0852
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-06 16:44:15 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 33.15 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -4,31 +4,32 @@ class Solution {
         int dIdx = n - 1;
         int pIdx = n - 1;
         
-        while(dIdx >= 0 || pIdx >= 0) {
+        while (dIdx >= 0 || pIdx >= 0) {
             while (dIdx >= 0 && deliveries[dIdx] == 0) dIdx--;
             while (pIdx >= 0 && pickups[pIdx] == 0) pIdx--;
             if (dIdx < 0 && pIdx < 0) break;
             
-            int dist = Math.max(dIdx, pIdx) + 1;
+            long dist = Math.max(pIdx, dIdx) + 1;
             answer += dist * 2L;
-                
+            
             int capD = cap;
             int capP = cap;
             
-            for (int i = dIdx; i >= 0 && capD > 0; i--) {
-                if (deliveries[i] == 0) continue;
-                int take = Math.min(capD, deliveries[i]);
-                deliveries[i] -= take;
-                capD -= take;
+            for (int i = dIdx; i >= 0; i--) {
+                if (capD <= 0) break;
+                if (deliveries[i] == 0 || capD <= 0) continue;
+                int takeD = Math.min(capD, deliveries[i]);
+                deliveries[i] -= takeD;
+                capD -= takeD;
             }
             
-            for (int i = pIdx; i >= 0 && capP > 0; i--) {
+            for (int i = pIdx; i >= 0; i--) {
+                if (capP <= 0) break;
                 if (pickups[i] == 0) continue;
-                int take = Math.min(capP, pickups[i]);
-                pickups[i] -= take;
-                capP -= take;
+                int takeP = Math.min(capP, pickups[i]);
+                pickups[i] -= takeP;
+                capP -= takeP;
             }
-            
         }
         
         return answer;
