f6d80ce44a5ac5fd88a15d1ab03462427a674b1b
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-15 15:15:05 +0900

    [level 2] Title: 피로도, Time: 5.88 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 84.2 MB, 시간: 3.46 ms
+메모리: 74.5 MB, 시간: 5.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 28일 13:29:34
+2025년 12월 15일 15:15:03
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,24 @@
 class Solution {
     
-    int minFatigue = Integer.MAX_VALUE;
-    int minCnt = 0;
+    int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        dfs(k, new boolean[dungeons.length], dungeons, 0);
+        dfs(0, k, dungeons, new boolean[dungeons.length], 0);    
         
-        return minCnt;
+        return answer;
     }
     
-    private void dfs(int k, boolean[] visited, int[][] dungeons, int cnt) {
-        minCnt = Math.max(minCnt, cnt);
-            
+    private void dfs(int idx, int fatigue, int[][] dungeons, boolean[] visited, int cnt) {    
+        answer = Math.max(answer, cnt);
+        
         for (int i = 0; i < dungeons.length; i++) {
-            int need = dungeons[i][0];
-            int consume = dungeons[i][1];
+            int minFatigue = dungeons[i][0];
+            int waste = dungeons[i][1];
             
             if (visited[i]) continue;
-            
-            if (k >= need) {
+            if (fatigue >= minFatigue && fatigue >= waste) {
                 visited[i] = true;
-                dfs(k - consume, visited, dungeons, cnt + 1);
+                dfs(i + 1, fatigue - waste, dungeons, visited, cnt + 1);
                 visited[i] = false;
             }
         }
