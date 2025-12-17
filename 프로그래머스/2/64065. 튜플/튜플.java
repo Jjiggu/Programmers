213a1249077d2753b82f1429516b31e138875e9e
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-17 15:54:58 +0900

    [level 2] Title: 튜플, Time: 56.32 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.substring(2, s.length() - 2);
-         
-        String[] sub = s.split("\\},\\{");        
-        Arrays.sort(sub, (o1, o2) -> o1.length() - o2.length());
+        String[] sub = preProcess(s);
         
         List<Integer> answer = new ArrayList<>();
         Set<Integer> seen = new HashSet<>();
         
         for (String tuple : sub) {
             String[] nums = tuple.split(",");
-            int[] arr = new int[nums.length];
-
-            for (int i = 0; i < nums.length; i++) {
-                int num = Integer.parseInt(nums[i]);
+            
+            for (String n : nums) {
+                int num = Integer.parseInt(n);
                 if (!seen.contains(num)) answer.add(num);
                 seen.add(num);
-            } 
+            }      
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    private String[] preProcess(String s) {
+        s = s.substring(2, s.length() - 2);
+        String[] sub = s.split("\\},\\{");
+        Arrays.sort(sub, (o1, o2) -> o1.length() - o2.length());
+        
+        return sub;
     }
 }
