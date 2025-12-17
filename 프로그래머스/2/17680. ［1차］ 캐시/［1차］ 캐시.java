c9dbc06906d17d4654c71ab9dbfb8612aabc85a5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-17 14:04:12 +0900

    [level 2] Title: [1차] 캐시, Time: 71.45 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,42 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        
-        List<String> cache = new ArrayList<>();
+        Deque<String> q = new ArrayDeque<>();
+        cities = cnvertUpperCase(cities);
         int answer = 0;
         
-        if (cacheSize == 0) return cities.length * 5;
-        
         for (String city : cities) {
-            city = city.toLowerCase();
-                
-            if (cache.size() == 0) {
-                cache.add(city);
-                answer += 5;
-            }
-            
-            if (cache.size() < cacheSize) {
-                if (cache.contains(city)) {
-                    cache.remove(city);
-                    cache.add(city);
+            if (q.size() < cacheSize) {
+                if (q.contains(city)) {  // 캐싱되어 있는 경우
                     answer += 1;
+                    q.remove(city);
+                    q.offer(city);
                 } else {
-                    cache.add(city);
                     answer += 5;
+                    q.offer(city);
                 }
             } else {
-                if (cache.contains(city)) {
-                    cache.remove(city);
-                    cache.add(city);
+                if (q.contains(city)) {  // 캐싱되어 있는 경우
                     answer += 1;
+                    q.remove(city);
+                    q.offer(city);
                 } else {
-                    cache.remove(0);
-                    cache.add(city);
                     answer += 5;
+                    q.offer(city);
+                    q.poll();
                 }
             }
         }
         
-        return answer - 1;
+        return answer;
+    }
+    
+    private String[] cnvertUpperCase(String[] cities) {
+        String[] arr = new String[cities.length];
+        int idx = 0;
+        
+        for (String city : cities) arr[idx++] = city.toUpperCase();
+        
+        return arr;
     }
 }
