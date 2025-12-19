fc8c3178710d34fb1a38787bfdbacc89508fbaab
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-19 13:21:13 +0900

    [level 2] Title: 프로세스, Time: 1.86 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 3.46 ms
+메모리: 80.7 MB, 시간: 1.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 20일 22:16:42
+2025년 12월 19일 13:21:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,53 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public static Integer findMax(Queue<int[]> queue) {
-        if (queue.isEmpty()) {
-            return null;
+    
+    class Process {
+        int priority;
+        int idx;
+        
+        Process(int priority, int idx) {
+            this.priority = priority;
+            this.idx = idx;
         }
-
-        int max = Integer.MIN_VALUE;
-
-        for (int[] arr : queue) {
-            if (arr[0] > max) { 
-                max = arr[0];
-            }
-        }
-
-        return max;
     }
     
+    TreeSet<Integer> maxSet = new TreeSet<>(Collections.reverseOrder());
+    Map<Integer, Integer> map = new HashMap<>();
     
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        ArrayList<Integer> prioritiesList = new ArrayList<Integer>();
-        Queue<int[]> q = new LinkedList<>();
-        
+        Deque<Process> q = new ArrayDeque<>();
         
         for (int i = 0; i < priorities.length; i++) {
-            q.add(new int[]{priorities[i], i});
+            int num = priorities[i];
+            q.offer(new Process(num, i));
+            maxSet.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        
-        while(!q.isEmpty()) {
-            int[] current = q.remove();
-            int currentPriority = current[0];
-            int currentIndex = current[1];
-            Integer maxNum = findMax(q);
-
-            if (maxNum != null && currentPriority < maxNum) {
-                q.add(current);
+        int curMax = maxSet.pollFirst();
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            Process cur = q.poll();
+            int priority = cur.priority;
+            int idx = cur.idx;
+            
+            if (priority == curMax) {
+                if (idx == location) return cnt + 1;
+                map.put(priority, map.get(priority) - 1);
+                cnt++;
+                if (map.get(priority) == 0) curMax = maxSet.pollFirst();
             } else {
-                prioritiesList.add(currentPriority);
-                answer++; 
-                
-                if (currentIndex == location) { 
-                    return answer;
-                }
-                
+                q.offer(cur);
             }
         }
         
-        return answer;
+        return priorities.length;
     }
 }
