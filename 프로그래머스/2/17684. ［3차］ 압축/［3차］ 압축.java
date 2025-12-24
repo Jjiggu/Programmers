a18e80a70e0ba6106684012e82de960182a91ded
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-24 13:47:48 +0900

    [level 2] Title: [3차] 압축, Time: 25.13 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,46 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        Map<String, Integer> dict = setAlphaMap();
+        Map<String, Integer> idxMap = ceateAlphaMap();
         List<Integer> answer = new ArrayList<>();
-        
-        // 문자 하나씩 늘리면서 dict에 있는지 확인 
-        // 있으면 길이 + 1
-        // 없으면 현재 단어 dict에 추가하고 바로 전 단어 인덱스 기록 
-        // 반복 
-        int left = 0;
-        int right = left + 1;
-        int lastIdx = 26;
+        int newIdx = 27;
         int n = msg.length();
-        String prev = "";;
         
-        while (left < n) {
-            String sub = msg.substring(left, right);
+        int i = 0;
+        while (i < n) {
+            String w = msg.substring(i, i + 1);
+            int wLen = 1;
             
-            if (right <= n && dict.containsKey(sub)) {
-                prev = sub;
-                
-                if (right == n) {
-                    answer.add(dict.get(prev));
+            for (int j = i + 1; j < n; j++) {
+                String next = msg.substring(i, j + 1);
+
+                if (idxMap.containsKey(next)) {
+                    w = next;
+                    wLen = next.length();
+
+                    continue;
+                } else {
+                    idxMap.put(next, newIdx++);
                     break;
                 }
-                right++;
-            } else {
-                answer.add(dict.get(prev));
-                
-                String newSub = msg.substring(left, right);
-                dict.put(newSub, ++lastIdx);
-                
-                left += prev.length();
-                right = left + 1;
-                prev = "";
-            }
+            }   
+            answer.add(idxMap.get(w));
+            i += w.length();
         }
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    private Map<String, Integer> setAlphaMap() {
+    
+    // 알파벳 목록 초기화 
+    private Map<String, Integer> ceateAlphaMap() {  
         Map<String, Integer> dict = new HashMap<>();
         int idx = 1;
         
