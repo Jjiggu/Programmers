3beda00c24b1b045538d296af6fa3c8b2c4c838b
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-24 13:59:40 +0900

    [level 2] Title: [3차] 압축, Time: 13.05 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -4,10 +4,10 @@ class Solution {
     public int[] solution(String msg) {
         Map<String, Integer> idxMap = ceateAlphaMap();
         List<Integer> answer = new ArrayList<>();
-        int newIdx = 27;
+        int lastIdx = 26;
         int n = msg.length();
-        
         int i = 0;
+        
         while (i < n) {
             String w = msg.substring(i, i + 1);
             int wLen = 1;
@@ -18,10 +18,9 @@ class Solution {
                 if (idxMap.containsKey(next)) {
                     w = next;
                     wLen = next.length();
-
                     continue;
                 } else {
-                    idxMap.put(next, newIdx++);
+                    idxMap.put(next, ++lastIdx);
                     break;
                 }
             }   
@@ -38,9 +37,7 @@ class Solution {
         Map<String, Integer> dict = new HashMap<>();
         int idx = 1;
         
-        for (char c = 'A'; c <= 'Z'; c++) {
-            dict.put(String.valueOf(c), idx++);
-        }
+        for (char c = 'A'; c <= 'Z'; c++) dict.put(String.valueOf(c), idx++);
         
         return dict;
     }
