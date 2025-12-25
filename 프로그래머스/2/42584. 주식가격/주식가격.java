99b09232b278320b36d553c8c0c1ff44a0065e21
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-25 14:28:01 +0900

    [level 2] Title: 주식가격, Time: 31.69 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,16 +7,16 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         
         for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            
-            for (int j = i + 1; j < n; j++) {        
-                if (prices[j] < prices[i]) {
-                    answer[i] = j - i;
-                    break;
-                } else {
-                    answer[i] = n - (i + 1);
-                }
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
         }
         
         return answer;
