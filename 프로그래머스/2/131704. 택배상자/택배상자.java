207e6f8519cda47bfe4702158ec9fcbd80574db8
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-29 15:13:11 +0900

    [level 2] Title: 택배상자, Time: 97.51 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,20 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int cnt = 0;
         Deque<Integer> stack = new ArrayDeque<>();
-
-        // 메인 벨트에서 상자 도착
-        for (int box = 1; box <= order.length; box++) {
-            stack.push(box);
-
-            // 트럭에 실을 수 있을 때까지 보조 벨트에서 pop
-            while (!stack.isEmpty() && stack.peek() == order[cnt]) {
-                stack.pop();
-                cnt++;
+        int answer = 0;
+        int idx = 0;
+        
+        for (int i = 1; i <= order.length; i++) {
+            // System.out.println(stack);
+            // 현재 order와 같으면 택배 실어야 됨
+            if (i == order[idx]) {
+                idx++;
+                answer++;
+            } else if (!stack.isEmpty() && stack.peek() == order[idx]) {
+                while (!stack.isEmpty() && stack.peek() == order[idx]) {
+                    answer++;
+                    idx++;
+                    stack.pop();   
+                }
+                stack.push(i);
+            } else { // 다른 경우 stack에 삽입 
+                stack.push(i);   
             }
         }
-
-        return cnt;
+        
+        
+        while (!stack.isEmpty()) {
+            if (stack.peek() == order[idx]) {
+                answer++;
+                idx++;
+                stack.pop();
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
     }
 }
