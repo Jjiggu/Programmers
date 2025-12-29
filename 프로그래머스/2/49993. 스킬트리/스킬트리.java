a47878ecfc3f75530ef1057f44d7471b32ad6fb7
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-29 13:57:03 +0900

    [level 2] Title: 스킬트리, Time: 0.10 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,22 +1,19 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int valid = 0;
-        
+        int answer = 0;
         char[] skillArr = skill.toCharArray();
         
         for (String tree : skill_trees) {
-            String filtered = skillFiltered(skill, tree);
-            
-            if (skill.startsWith(filtered)) valid++;
+            String filtered = filterBySkillOrder(skill, tree);
+            if (skill.startsWith(filtered)) answer++;
         }
         
-        
-        return valid;
+        return answer;
     }
-
-    private String skillFiltered(String skill, String tree) {
-        
+    
+    private String filterBySkillOrder(String skill, String tree) {
         StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < tree.length(); i++) {
             char alpha = tree.charAt(i);
             if (skill.indexOf(alpha) != -1) sb.append(alpha);
