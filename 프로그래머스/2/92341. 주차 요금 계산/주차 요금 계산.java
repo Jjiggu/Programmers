27f51830add1f7d5ad9659f7ae6f79488f144d0a
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2025-12-29 15:54:56 +0900

    [level 2] Title: 주차 요금 계산, Time: 8.98 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,21 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        
         Map<String, int[]> map = initMap(records);
         int[] answer = new int[map.size()];
-        
         int maxTime = 23 * 60 + 59;
         int idx = 0;
+        
         for (int[] parking : map.values()) {
             if (parking[0] != -1) parking[1] += maxTime - parking[0];
-            answer[idx++] = calFee(fees, parking);
+            answer[idx++] = calcFee(fees, parking);
         }
-        
         return answer;
     }
     
-    private int calFee(int[] fees, int[] parking) {
+    private int calcFee(int[] fees, int[] parking) {
         int basicTime = fees[0];
         int basicFare = fees[1];
         int unitTime = fees[2];
@@ -41,10 +39,9 @@ class Solution {
             String carNum = r[1];
             String inOut = r[2];
             
-            if (inOut.equals("IN")) {  // 입차인 경우
-                map.putIfAbsent(carNum, new int[]{0, 0});
-                map.get(carNum)[0] = time;
-            } else {  // 출차인 경우
+            if (inOut.equals("IN")) {
+                map.computeIfAbsent(carNum, k -> new int[2])[0] = time;
+            } else {
                 int[] val = map.get(carNum);
                 val[1] += time - val[0];
                 val[0] = -1;
@@ -56,7 +53,6 @@ class Solution {
     
     private int timeToInt(String time) {
         String[] t = time.split(":");
-        
         return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
     }
 }
