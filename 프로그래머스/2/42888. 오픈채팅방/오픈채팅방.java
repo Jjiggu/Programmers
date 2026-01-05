a365adcdd3e97cbdd730a10c18dc0b8e10c6ccb6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-01-05 15:44:13 +0900

    [level 2] Title: 오픈채팅방, Time: 166.37 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        Map<String, String> name = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         
         for (String r : record) {
-            String[] tmp = r.split(" ");
-            if (tmp[0].equals("Enter")) {
-                name.put(tmp[1], tmp[2]);   
-            } else if (tmp[0].equals("Change")) {
-                name.put(tmp[1], tmp[2]);
+            String[] s = r.split(" ");
+            if (s[0].equals("Enter")) {
+                map.put(s[1], s[2]);
+            } else if (s[0].equals("Change")) {
+                map.put(s[1], s[2]);
             }
         }
         
-        ArrayList<String> answer = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
         for (String r : record) {
-            String[] tmp = r.split(" ");
-            if (tmp[0].equals("Enter")) {
-                answer.add(name.get(tmp[1]) + "님이 들어왔습니다.");
-            } else if (tmp[0].equals("Leave")) {
-                answer.add(name.get(tmp[1]) + "님이 나갔습니다.");
+            String[] s = r.split(" ");
+            if (s[0].equals("Enter")) {
+                answer.add(map.get(s[1]) + "님이 들어왔습니다.");
+            } else if (s[0].equals("Leave")) {
+                answer.add(map.get(s[1]) + "님이 나갔습니다.");
             }
         }
         
