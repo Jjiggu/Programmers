15a36f1094dc820fd85a25169fa34bd4115fa6d7
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-01-12 15:04:21 +0900

    [level 2] Title: 배달, Time: 18.49 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-class Solution {
-    
+class Solution {    
     static class Node {
         int v;
         int cost;
@@ -12,20 +11,21 @@ class Solution {
         }
     }
     
-    static ArrayList<Node>[] graph;
+    static List<Node>[] graph;
     
     public int solution(int N, int[][] road, int K) {
-        
         graph = new ArrayList[N + 1];
-        
         for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
         
         for (int[] r : road) {
-            graph[r[0]].add(new Node(r[1], r[2]));
-            graph[r[1]].add(new Node(r[0], r[2]));
+            int u = r[0];
+            int v = r[1];
+            int cost = r[2];
+            graph[u].add(new Node(v, cost));
+            graph[v].add(new Node(u, cost));
         }
         
-        int[] dist = dijkstra(N, 1);    
+        int[] dist = dijkstra(N, 1);
         int answer = 0;
         
         for (int d : dist) if (d <= K) answer++;
@@ -34,7 +34,6 @@ class Solution {
     }
     
     private int[] dijkstra(int N, int start) {
-        
         int[] dist = new int[N + 1];
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
         
@@ -43,13 +42,15 @@ class Solution {
         dist[start] = 0;
         
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            Node cur = pq.poll();
             
-            if (dist[now.v] < now.cost) continue;
+            if (dist[cur.v] < cur.cost) continue;
             
-            for (Node next : graph[now.v]) {
-                if (dist[next.v] > now.cost + next.cost) {
-                    dist[next.v] = now.cost + next.cost;
+            for (Node next : graph[cur.v]) {
+                int curCost = cur.cost + next.cost;
+                
+                if (dist[next.v] > curCost) {
+                    dist[next.v] = curCost;
                     pq.add(new Node(next.v, dist[next.v]));
                 }
             }
