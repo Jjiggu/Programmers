44e9d811e0dd96a6c83a2814406eb0c08b1d3360
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-01-26 16:29:46 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 23.43 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -5,26 +5,24 @@ class Solution {
         
         int[][] dp = new int[n][m];
         
-        for (int i = 0; i < m; i ++) {
+        for (int i = 0; i < m; i++) {
             if (board[0][i] == 1) dp[0][i] = 1;
         }
-        for (int i = 0; i < n; i ++) {
+        
+        for (int i = 0; i < n; i++) {
             if (board[i][0] == 1) dp[i][0] = 1;
         }
         
-        
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                if (board[i][j] == 1) {
-                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
-                } else {
-                    dp[i][j] = 0;
-                }
+                if (board[i][j] == 1) dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                else dp[i][j] = 0;
             }
         }
         
         int answer = findMax(dp);
         return (int)Math.pow(answer, 2);
+        
     }
     
     public int findMax(int[][] dp) {
