06c2bcafac6185203eb3825f7e10c52201380b9e
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-02-04 08:41:47 +0900

    [level 2] Title: 숫자 블록, Time: 301.44 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -3,6 +3,10 @@ class Solution {
     private static final int LIMIT = 10_000_000;
     
     public int[] solution(long begin, long end) {
+        // 짝수인 경우 /2 값이 제일 큰 수 
+        // 소수인 경우 1
+        // 홀수인 경우 자신을 제외한 가장 큰 약수
+        
         int n = (int)(end - begin + 1);
         int[] answer = new int[n];
         
@@ -10,25 +14,26 @@ class Solution {
             long num = begin + i;
             
             if (num == 1) answer[i] = 0;
-            else answer[i] = findPrime(num);
+            else answer[i] = findNum(num);
         }
         
         return answer;
     }
     
-    private int findPrime(long n) {
-        long sqrt = (long)Math.sqrt(n);
-        int res = 1;
+    private int findNum(long num) {
+        long maxRange = (long)Math.sqrt(num);
+        int maxNum = 1;
         
-        for (long i = 2; i <= sqrt; i++) {
-            if (n % i == 0) {
-                long pair = n / i;
+        for (long i = 2; i <= maxRange; i++) {
+            if (num % i == 0) {
+                long pair = num / i;
                 
-                if (pair <= LIMIT) return (int)pair;
-                if (i <= LIMIT) res = (int)i;
+                if (pair <= LIMIT) return (int) pair;
+                else maxNum = (int)i;
+                // if (i <= LIMIT) maxNum = (int)i;
             }
         }
         
-        return res;
+        return maxNum;
     }
 }
