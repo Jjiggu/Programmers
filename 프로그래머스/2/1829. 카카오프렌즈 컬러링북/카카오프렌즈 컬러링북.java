e6751ecc7b3310518d19b76a6c77d41a98f3662f
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-02-23 15:25:27 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 23.60 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -1,59 +1,58 @@
 import java.util.*;
 
 class Solution {
-    private int m, n;
-    private int[][] pic;
-    private boolean[][] visited;
-    private final int[] dx = {-1, 1, 0, 0};
-    private final int[] dy = {0, 0, -1, 1};
-
+    
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    int maxSizeOfOneArea = 0;
+    
     public int[] solution(int m, int n, int[][] picture) {
-        this.m = m;
-        this.n = n;
-        this.pic = picture;
-        visited = new boolean[m][n];
-
         int numberOfArea = 0;
-        int maxSizeOfOneArea = 0;
+        int[] answer = new int[2];
 
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && pic[i][j] != 0) {
-                    int size = bfs(i, j);
+                if (picture[i][j] != 0) {
+                    bfs(i, j, m, n, picture[i][j], picture);
+                    picture[i][j] = 0;
                     numberOfArea++;
-                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, size);
                 }
             }
         }
-
+        
         return new int[]{numberOfArea, maxSizeOfOneArea};
     }
-
-    private int bfs(int startX, int startY) {
-        int color = pic[startX][startY];
+    
+    private int bfs(int startX, int startY, int m, int n, int target, int[][] picture) {
+        boolean[][] visited = new boolean[m][n];
         Deque<int[]> q = new ArrayDeque<>();
-        visited[startX][startY] = true;
+        int cnt = 1;
+        
         q.add(new int[]{startX, startY});
-
-        int size = 0;
-
+        visited[startX][startY] = true;
+        
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            size++;
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
-
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    if (!visited[nx][ny] && pic[nx][ny] == color) {
-                        visited[nx][ny] = true;
-                        q.add(new int[]{nx, ny});
-                    }
-                }
+            int x = cur[0];
+            int y = cur[1];
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+                if (visited[nx][ny] || picture[nx][ny] != target) continue;
+                
+                picture[nx][ny] = 0;
+                q.add(new int[]{nx, ny});
+                visited[nx][ny] = true;
+                cnt++;
             }
         }
-
-        return size;
+        
+        maxSizeOfOneArea = Math.max(maxSizeOfOneArea, cnt);
+        
+        return cnt;
     }
 }
