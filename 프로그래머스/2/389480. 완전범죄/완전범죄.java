1bb1dd53bea91abe1f456f9754ab329be115f2c6
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-02-23 14:54:20 +0900

    [level 2] Title: 완전범죄, Time: 0.98 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,28 +1,28 @@
-class Solution {
-    static int answer = Integer.MAX_VALUE;
+class Solution {    
+    
+    int answer = Integer.MAX_VALUE;
     
     public int solution(int[][] info, int n, int m) {
         boolean[][][] visited = new boolean[info.length][n][m];
-        
-        dfs(0, info, n, m, 0, 0, visited);
+        dfs(0, 0, 0, n, m, info, visited);
         
         return answer == Integer.MAX_VALUE ? -1 : answer;
     }
     
-    private void dfs(int k, int[][] info, int n, int m, int a, int b, boolean[][][] visited) {
-        if (a >= n || b >= m) return;
+    private void dfs(int k, int ACnt, int BCnt, int n, int m, int[][] info, boolean[][][] visited) {
+        if (ACnt >= n || BCnt >= m) return;
         
         if (k == info.length) {
-            answer = Math.min(answer, a);
+            answer = Math.min(answer, ACnt);
             return;
         }
         
-        if (a >= answer) return;
-        if (visited[k][a][b]) return;
+        if (ACnt >= answer) return;
+        if (visited[k][ACnt][BCnt]) return;
         
-        visited[k][a][b] = true;
+        visited[k][ACnt][BCnt] = true;
         
-        dfs(k + 1, info, n, m, a + info[k][0], b, visited);
-        dfs(k + 1, info, n, m, a, b + info[k][1], visited);
+        dfs(k + 1, ACnt + info[k][0], BCnt, n, m, info, visited);  // A가 훔치는 경우
+        dfs(k + 1, ACnt, BCnt + info[k][1], n, m, info, visited);  // B가 훔치는 경우
     }
 }
