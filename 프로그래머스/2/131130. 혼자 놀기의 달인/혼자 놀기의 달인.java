86e6f694e13a6e53d839e3ba52b46bfe19e16e58
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-05 15:40:52 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.89 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,29 +1,42 @@
 import java.util.*;
 
 class Solution {
+    
+    boolean[] visited;
+    List<List<Integer>> answer = new ArrayList<>();
+    
     public int solution(int[] cards) {
         int n = cards.length;
-        boolean[] visited = new boolean[n];
-        List<Integer> answer = new ArrayList<>();
+        visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
-            
-            int cur = i;
-            int cnt = 0;
-            
-            while (!visited[cur]) {
-                visited[cur] = true;
-                cnt++;
-                cur = cards[cur] - 1;
+            if (!visited[i]) {
+                play(cards[i] - 1, cards);
             }
-            
-            if (cnt > 0) answer.add(cnt);
         }
         
-        if (answer.size() < 2) return 0;
-        answer.sort(Collections.reverseOrder());
+        Collections.sort(answer, (o1, o2) -> o2.size() - o1.size());
         
-        return answer.get(0) * answer.get(1);
+        return answer.size() == 1 ? 0 : answer.get(0).size() * answer.get(1).size();
+    }
+    
+    private void play(int idx, int[] cards) {
+        
+        List<Integer> list = new ArrayList<>();
+        int nowIdx = idx;      
+        int nextIdx = cards[nowIdx] - 1;
+        
+        while(true) {
+            if (visited[nowIdx]) {
+                answer.add(list);
+                return;
+            }
+            
+            visited[nowIdx] = true;            
+            list.add(nowIdx);
+            
+            nowIdx = nextIdx;
+            nextIdx = cards[nextIdx] - 1;
+        }
     }
 }
