16b3eee201eb6c49fda31061fa1a495c57ccc5b9
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-10 15:47:55 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.25 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,62 +1,52 @@
 class Solution {
     public int solution(String[] board) {
-        int OCnt = 0;
-        int XCnt = 0;
+        int xCnt = 0;
+        int oCnt = 0;
         
-        for (String s : board) {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == 'O') OCnt++;
-                if (s.charAt(i) == 'X') XCnt++;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i].charAt(j) == 'O') oCnt++;
+                else if (board[i].charAt(j) == 'X') xCnt++;
             }
         }
         
-        // 기본 규칙
-        if (XCnt > OCnt) return 0;
-        if (OCnt - XCnt > 1) return 0;
+        if (oCnt == xCnt + 1) {
+            // 개수가 하나 차이나는 경우 -> O가 이겨야 함 
+            boolean oWin = isBingo(board, 'O');
+            boolean xWin = isBingo(board, 'X');
+            
+            if (oWin && !xWin) return 1;
+            if (!oWin && !xWin) return 1;
+        }
         
-        boolean OWin = isWin(board, 'O');
-        boolean XWin = isWin(board, 'X');
+        // 개수가 같은 경우 -> X가 이기고 O는 빙고이면 안 됨
+        if (oCnt == xCnt) {
+            if (oCnt < 3 && xCnt < 3) return 1;
+            
+            boolean oWin = isBingo(board, 'O');
+            boolean xWin = isBingo(board, 'X');
+            
+            if (!oWin && xWin) return 1;
+            if (!oWin && !xWin) return 1;
+        }
         
-        // 2. 둘 다 이긴 경우는 불가능
-        if (OWin && XWin) return 0;
-        
-        // 3. O가 이긴 경우: O는 항상 X보다 한 수 더 많이 둔 상태
-        if (OWin && OCnt != XCnt + 1) return 0;
-        
-        // 4. X가 이긴 경우: X는 항상 O와 같은 수를 둔 상태
-        if (XWin && OCnt != XCnt) return 0;
-        
-        return 1;
+        return 0;  // 개수가 맞지 않는 경우 
     }
     
-    private boolean isWin(String[] board, char target) {
+    private boolean isBingo(String[] board, Character target) {
+        // 가로 
         for (int i = 0; i < 3; i++) {
-            if (board[i].charAt(0) == target &&
-                board[i].charAt(1) == target &&
-                board[i].charAt(2) == target) {
-                return true;
-            }
+            if (board[i].charAt(0) == target && board[i].charAt(1) == target && board[i].charAt(2) == target) return true;
         }
         
-        for (int j = 0; j < 3; j++) {
-            if (board[0].charAt(j) == target &&
-                board[1].charAt(j) == target &&
-                board[2].charAt(j) == target) {
-                return true;
-            }
+        // 세로 
+        for (int i = 0; i < 3; i++) {
+            if (board[0].charAt(i) == target && board[1].charAt(i) == target && board[2].charAt(i) == target) return true;
         }
         
-        if (board[0].charAt(0) == target &&
-            board[1].charAt(1) == target &&
-            board[2].charAt(2) == target) {
-            return true;
-        }
-
-        if (board[0].charAt(2) == target &&
-            board[1].charAt(1) == target &&
-            board[2].charAt(0) == target) {
-            return true;
-        }
+        // 대각선 
+        if (board[0].charAt(0) == target && board[1].charAt(1) == target && board[2].charAt(2) == target) return true;
+        if (board[0].charAt(2) == target && board[1].charAt(1) == target && board[2].charAt(0) == target) return true;
         
         return false;
     }
