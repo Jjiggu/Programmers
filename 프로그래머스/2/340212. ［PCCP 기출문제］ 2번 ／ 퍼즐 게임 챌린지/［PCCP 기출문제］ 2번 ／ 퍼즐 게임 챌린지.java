29d6ea27b5e862f154053f3effae0bfadf03a1b2
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-19 15:43:29 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 37.58 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,38 +1,47 @@
-class Solution { 
-    public int solution(int[] diffs, int[] times, long limit) { 
-        return bs(diffs, times, limit); 
-    } 
-    private int bs(int[] diffs, int[] times, long limit) { 
-        int left = 1; 
-        int right = Integer.MAX_VALUE; 
-        while (left < right) { 
-            int mid = left + (right - left) / 2;
-            if (solveSimul(mid, diffs, times, limit)) { 
-                right = mid; 
-            } else { 
-                left = mid + 1; 
-            } 
-        } 
-        
-        return left; 
-    } 
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        return bs(diffs, times, limit);
+    }
     
-    private boolean solveSimul(int proficiency, int[] diffs, int[] times, long limit) { 
-        long time = 0L; 
+    private int bs(int[] diffs, int[] times, long limit) {
+        int left = 1;
+        int right = 300_001;
         
-        for (int i = 0; i < diffs.length; i++) { 
-            int reqDiff = diffs[i]; 
-            int reqTime = times[i]; 
-            if (proficiency >= reqDiff) {  // 퍼즐 풀 수 있는 경우
-                time += reqTime; 
-            } else {  // 퍼즐 풀 수 없는 경우
-                int digit = reqDiff - proficiency;
-                int prevTime = i > 0 ? times[i - 1] : 0;
-                int curTime = times[i];
-                time += (long)(curTime + prevTime) * digit + curTime; 
-            } if (time > limit) return false; 
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (solve(diffs, times, mid, limit)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean solve(int[] diffs, int[] times, int level, long limit) {
+        long timeRequired = 0;
+        int n = diffs.length;
+        
+        timeRequired += times[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (timeRequired > limit) return false;
+            
+            int diff = diffs[i];
+            int timeCur = times[i];
+            int timePrev = times[i - 1];
+            
+            if (diff <= level) {
+                timeRequired += timeCur;
+            } else {
+                int wrong = diff - level;
+                int timeTaken = timeCur + timePrev;
+                timeRequired += (wrong * timeTaken + timeCur);
+            }
         } 
         
-        return true; 
-    } 
+        return timeRequired <= limit;
+    }
 }
