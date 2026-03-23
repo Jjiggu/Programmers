21a7f7ca7850a8b6a3915a022e1fb9d2116b223d
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-23 22:18:55 +0900

    [level 2] Title: 광물 캐기, Time: 1.83 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,48 +1,85 @@
 import java.util.*;
 
 class Solution {
+    
+    static class Group {
+        int dia;
+        int iron;
+        int stone;
+        
+        public Group(int dia, int iron, int stone) {
+            this.dia = dia;
+            this.iron = iron;
+            this.stone = stone;
+        }
+    }
+    
     public int solution(int[] picks, String[] minerals) {
-        int[][] tired = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
-        String[] pickTypes = {"diamond", "iron", "stone"};
+        int maxMinerals = getMaxMinerals(picks);
+        List<Group> mineralList = cntMinerals(minerals, maxMinerals);
         
-        Map<String, Integer> mineralIdx = new HashMap<>();
-        int maxPick = 0;
+        mineralList.sort((o1, o2) -> {
+            if (o1.dia != o2.dia) return o2.dia - o1.dia;
+            if (o1.iron != o2.iron) return o2.iron - o1.iron;
+            return o2.stone - o1.stone;
+        });
         
-        for (int i = 0; i < picks.length; i++) {
-            mineralIdx.put(pickTypes[i], i);  // 인덱스 매핑
-            maxPick += picks[i] * 5;  // 캘 수 있는 최대 광물 수
-        }
         
-        int mineralCnt = Math.min(maxPick, minerals.length);  // 실제로 캘 광물의 수
-        List<int[]> costList = new ArrayList<>();
-        for (int i = 0; i < mineralCnt; i+=5) {
-            int[] costs = new int[3];  // 곡괭이당 피로도 전부 계산
-            for (int j = i; j < i + 5 && j < mineralCnt; j++) {
-                for (int k = 0; k < 3; k++) {
-                    costs[k] += tired[k][mineralIdx.get(minerals[j])];
-                }
-            }
-            costList.add(costs);
-        }
-        
-        costList.sort((o1, o2) -> o2[2] - o1[2]);  // 돌 기준 정렬
-        
+        return mining(mineralList, picks);
+    }
+    
+    private int mining(List<Group> mineralList, int[] picks) {
         int answer = 0;
-        for (int[] cost : costList) {
-            int minCost = Integer.MAX_VALUE;
-            int idx = -1;
-            for (int i = 0; i < cost.length; i++) {
-                if (picks[i] > 0 && cost[i] < minCost) {  // 피로도 가장 적게드는 곡괭이 사용
-                    minCost = cost[i];
-                    idx = i;
-                }
-            }
-            picks[idx]--;
-            answer += minCost;
-            
-            if (idx == -1) break;
+        
+        for (Group g : mineralList) {
+            if (picks[0] > 0) {
+                picks[0]--;
+                answer += calcTired(g, 0);
+            } else if (picks[1] > 0) {
+                picks[1]--;
+                answer += calcTired(g, 1);
+            } else if (picks[2] > 0) {
+                picks[2]--;
+                answer += calcTired(g, 2);
+            } 
         }
         
         return answer;
+    }
+    
+    private int calcTired(Group g, int type) {
+        int dia = g.dia;
+        int iron = g.iron;
+        int stone = g.stone;
+        
+        if (type == 0) return dia + iron + stone;
+        else if (type == 1) return 5 * dia + iron + stone;
+        else return 25 * dia + 5 * iron + stone;
+    }
+    
+    private List<Group> cntMinerals(String[] minerals, int maxMinerals) {
+        List<Group> list = new ArrayList<>();
+        int limit = Math.min(minerals.length, maxMinerals);
+        
+        for (int i = 0; i < limit; i+=5) {
+            int diaCnt = 0;
+            int ironCnt = 0;
+            int stoneCnt = 0;
+            
+            for (int j = i; j < i + 5 && j < limit; j++) {
+                String mine = minerals[j];
+            
+                if (mine.equals("diamond")) diaCnt++;
+                else if (mine.equals("iron")) ironCnt++;
+                else stoneCnt++;   
+            }
+            list.add(new Group(diaCnt, ironCnt, stoneCnt));
+        }
+        
+        return list;
+    }
+    
+    private int getMaxMinerals(int[] picks) {
+        return (picks[0] + picks[1] + picks[2]) * 5;
     }
 }
