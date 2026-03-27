dce5fba8304d9399ffd8f4aa8b04552403329c35
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-27 13:13:11 +0900

    [level 2] Title: [3차] 방금그곡, Time: 15.65 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,74 +1,105 @@
 import java.util.*;
 
 class Solution {
+    String[] scale = {"C", "C#", "D", "D#", "E", "E#", "F", "F#", "G", "G#", "A", "A#", "B", "B#"};
+    Map<String, Integer> scaleMap;
+    
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
-        int bestDur = -1;
+        int maxPlayTime = -1;
         
-        Map<String, Integer> toneMap = new HashMap<>();
-        String[] tones = {"C","C#","D","D#","E", "E#", "F","F#","G","G#","A","A#","B", "B#"};
-        for (int i = 0; i < tones.length; i++) toneMap.put(tones[i], i);
+        // #처리를 해줘야하기 때문에 특정 숫자로 변환해서 확인 
+        initScale();
         
+        List<Integer> target = convertScale(m);
         
-        String convertM = convert(m, toneMap);
+        // musicinfos에서 실제 재생된 구간 변환 후 저장 
         for (String musicinfo : musicinfos) {
-            String[] tmp = musicinfo.split(",");
+            String[] info = musicinfo.split(",");
             
-            int dur = calcTime(tmp[0], tmp[1]);
-            String title = tmp[2];
-            String encPattern = convert(tmp[3], toneMap);
+            int playTime = timeToInt(info[0], info[1]);
+            String songName = info[2];
+            List<Integer> original = convertScale(info[3]);
+            // 실제 재생된 멜로디 생성
+            List<Integer> played = makePlayedMelody(original, playTime);
             
-            StringBuilder sb = new StringBuilder();
-            int times = dur / encPattern.length();
-            int rem   = dur % encPattern.length();
-            for (int k = 0; k < times; k++) sb.append(encPattern);
-            if (rem > 0) sb.append(encPattern, 0, rem);
-            String sound = sb.toString();
-            
-            if (sound.contains(convertM) && dur > bestDur) {
-                bestDur = dur;
-                answer = title;
-            } 
+            // target이 played 안에 연속 부분수열로 포함되는지 확인
+            if (containsMelody(played, target) && playTime > maxPlayTime) {
+                maxPlayTime = playTime;
+                answer = songName;
+            }
         }
         
+        // 포함되지 않으면 None 반환 
         return answer;
     }
     
-    private int calcTime(String start, String end) {
-        String[] startTime = start.split(":");
-        String[] endTime = end.split(":");
-        
-        return timeToInt(endTime) - timeToInt(startTime);
-    }
-    
-    private int timeToInt(String[] timeArr) {
-        int hour = Integer.parseInt(timeArr[0]);
-        int min = Integer.parseInt(timeArr[1]);
-        
-        return hour * 60 + min;
-    }
-    
-    private String convert(String m, Map<String,Integer> toneMap) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m.length(); ) {
-            String note = (i+1 < m.length() && m.charAt(i+1)=='#')
-                        ? m.substring(i, i+2)
-                        : m.substring(i, i+1);
-            int idx = toneMap.get(note);        
-            sb.append((char)('a' + idx));      
-            i += note.length();
-        }
-        return sb.toString();
-    }
+    private List<Integer> convertScale(String s) {
+        List<Integer> list = new ArrayList<>();
 
+        for (int i = 0; i < s.length();) {
+            String key;
+
+            if (i + 1 < s.length() && s.charAt(i + 1) == '#') {
+                key = s.substring(i, i + 2);
+                i += 2;
+            } else {
+                key = String.valueOf(s.charAt(i));
+                i++;
+            }
+
+            Integer value = scaleMap.get(key);
+            list.add(value);
+        }
+
+        return list;
+    }
     
-    private String repeatSound(String pattern, int cnt, int rem) {
-        StringBuilder sb = new StringBuilder();
+    // 실제 재생된 멜로디 만들기
+    private List<Integer> makePlayedMelody(List<Integer> original, int playTime) {
+        List<Integer> played = new ArrayList<>();
+
+        for (int i = 0; i < playTime; i++) {
+            played.add(original.get(i % original.size()));
+        }
+
+        return played;
+    }
+    
+    // played 안에 target이 연속 부분수열로 포함되는지 검사
+    private boolean containsMelody(List<Integer> played, List<Integer> target) {
+        if (target.size() > played.size()) return false;
         
-        for (int i = 0; i < cnt; i++) sb.append(pattern);
+        for (int i = 0; i <= played.size() - target.size(); i++) {
+            boolean same = true;
+            
+            for (int j = 0; j < target.size(); j++) {
+                if (!played.get(i + j).equals(target.get(j))){
+                    same = false;
+                    break;
+                }
+            }
+            
+            if (same) return true;
+        }
+
+        return false;
+    }
+    
+    private int timeToInt(String start, String end) {
+        String[] s = start.split(":");
+        String[] e = end.split(":");
         
-        if (rem > 0) sb.append(pattern.substring(0, rem));
+        int endTime = Integer.parseInt(e[0]) * 60 + Integer.parseInt(e[1]);
+        int startTime = Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
         
-        return sb.toString();
+        return endTime - startTime;
+    }
+    
+    private void initScale() {
+        scaleMap = new HashMap<>();
+        int idx = 1;
+        
+        for (String s : scale) scaleMap.put(s, idx++);
     }
 }
