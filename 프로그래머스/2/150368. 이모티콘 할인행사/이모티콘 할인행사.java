b1378b4b26c375a402e5984a5236eb9516914497
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-30 13:47:33 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 67.91 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -2,36 +2,37 @@ import java.util.*;
 
 class Solution {
     
-    int maxSubscribe = 0;
+    int maxSubscribers = 0;
     int maxSales = 0;
     int[] discountPer = {10, 20, 30, 40};
     
     public int[] solution(int[][] users, int[] emoticons) {
         dfs(0, new int[emoticons.length], users, emoticons);
         
-        return new int[]{maxSubscribe, maxSales};
+        return new int[]{maxSubscribers, maxSales};
     }
     
-    public void dfs(int k, int[] discounts, int[][] users, int[] emoticons) {
+    private void dfs(int k, int[] discounts, int[][] users, int[] emoticons) {
         if (k == emoticons.length) {
-            int subscriber = 0;
+            int subscribers = 0;
             int sales = 0;
             
             for (int[] user : users) {
                 int sum = 0;
-
+                
+                
                 for (int i = 0; i < emoticons.length; i++) {
                     if (discounts[i] >= user[0]) {
                         sum += emoticons[i] * (100 - discounts[i]) / 100;
                     }
                 }
-
-                if (sum >= user[1]) subscriber++;
+                
+                if (sum >= user[1]) subscribers++;
                 else sales += sum;
             }
             
-            if (subscriber > maxSubscribe || subscriber == maxSubscribe && sales > maxSales) {
-                maxSubscribe = subscriber;
+            if (subscribers > maxSubscribers || subscribers == maxSubscribers && sales > maxSales) {
+                maxSubscribers = subscribers;
                 maxSales = sales;
             }
             return;
