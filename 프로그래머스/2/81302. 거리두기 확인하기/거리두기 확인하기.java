2c859af3004cb2eeed9d614df053a9ddc2e1fb57
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-03-30 16:34:06 +0900

    [level 2] Title: 거리두기 확인하기, Time: 2.64 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,69 +1,92 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        int idx = 0;
+    
+    class Pos {
+        int x;
+        int y;
         
-        for (String[] place : places) {
-            List<int[]> pos = findPos(place);
-            if (isValid(pos, place)) answer[idx++] = 1;
-            else answer[idx++] = 0;
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    
+    public int[] solution(String[][] places) {
+        
+        int n = places.length;
+        int[] answer = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            String[] place = places[i];
+            List<Pos> posList = findPos(place);  // P 좌표 찾기
+            
+            if (isValid(posList, place)) answer[i] = 1;         
         }
         
         return answer;
     }
     
-    private boolean isValid(List<int[]> pos, String[] place) {
+    private boolean isValid(List<Pos> posList, String[] place) {
+        int n = posList.size();
         
-        for (int i = 0; i < pos.size(); i++) {
-            for (int j = i + 1; j < pos.size(); j++) {
-                int[] a = pos.get(i);
-                int[] b = pos.get(j);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Pos a = posList.get(i);
+                Pos b = posList.get(j);
                 
-                if (calcDist(a, b) > 2) continue;
-                if (!checkPartition(a, b, place)) return false;
+                if (calcDist(a, b) > 2) continue;  // 맨해튼 거리 2이하인지 확인 
+                if (!checkPartition(a, b, place)) return false;  // 거리 2이하인 경우 파티션 있는지 확인 
             }
         }
+        
         return true;
     }
     
-    private boolean checkPartition(int[] a, int[] b, String[] place) {
-        int r1 = a[0], r2 = b[0];
-        int c1 = a[1], c2 = b[1];
-        int d = calcDist(a, b);
+    private boolean checkPartition(Pos a, Pos b, String[] place) {
+        int r1 = a.x;
+        int r2 = b.x;
+        int c1 = a.y;
+        int c2 = b.y;
+        int dist = calcDist(a, b);
         
-        if (d == 0) return true;
-        if (d == 1) return false;
-        
-        if (d == 2) {
-            if (r1 == r2) { // 같은 행
+        if (dist == 1) return false;
+        if (dist == 2) {
+            if (r1 == r2) {  // 같은 행에 있는 경우 
                 int mc = (c1 + c2) / 2;
                 return place[r1].charAt(mc) == 'X';
-            } else if (c1 == c2) { // 같은 열
+            } else if (c1 == c2) {  // 같은 열에 있는 경우
                 int mr = (r1 + r2) / 2;
                 return place[mr].charAt(c1) == 'X';
-            } else { // 대각선
+            } else {  // 대각선에 있는 경우 
                 return place[r1].charAt(c2) == 'X' && place[r2].charAt(c1) == 'X';
-            }
+            }   
         }
-        
         
         return true;
     }
     
-    private int calcDist(int[] x, int[] y) {
-        return Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1]);
+    // 좌표로 맨해튼 거리 2이하인지 확인 
+    // 2이하라면 파티션 확인 
+    private int calcDist(Pos a, Pos b) {
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
     }
     
-    private List<int[]> findPos(String[] place) {
-        List<int[]> pos = new ArrayList<>();
+    private List<Pos> findPos(String[] place) {
+        int n = place.length;
+        int m = place[0].length();
+        List<Pos> list = new ArrayList<>();
         
-        for (int i = 0; i < place.length; i++) {
-            for (int j = 0; j < place[0].length(); j++) {
-                if (place[i].charAt(j) == 'P') pos.add(new int[]{i, j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (place[i].charAt(j) == 'P') list.add(new Pos(i, j));
             }
         }
-        return pos;
+        
+        return list;
     }
 }
