ae6dc7305bcb205ee393c77f64679a74dea3593d
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-04-28 15:55:59 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.57 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -3,26 +3,19 @@ class Solution {
         int n = sticker.length;
         int[] dp = new int[n + 1];
         
-        if (sticker.length == 1) {
-            return sticker[0];
-        }
+        if (n <= 1) return sticker[0];
         
+        // 첫 번째 스티커 선택하는 경우
         dp[0] = sticker[0];
         dp[1] = dp[0];
         
-        for (int i = 2; i < n - 1; i++) {
-            dp[i] = Math.max(sticker[i] + dp[i - 2], dp[i - 1]);
-        }
-        
+        for (int i = 2; i < n; i++) dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
         int answer = dp[n - 2];
         
-        
+        // 첫 번째 스티커 선택하지 않는 경우
         dp[0] = 0;
         dp[1] = sticker[1];
-        
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(sticker[i] + dp[i - 2], dp[i - 1]);
-        }
+        for (int i = 2; i < n; i ++) dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
         
         return Math.max(answer, dp[n - 1]);
     }
