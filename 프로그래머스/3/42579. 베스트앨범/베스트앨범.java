8ae269804fe09f3d60dda5f1639b788b5f87b197
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-04-28 15:34:38 +0900

    [level 3] Title: 베스트앨범, Time: 8.66 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,35 +1,21 @@
 import java.util.*;
 
 class Solution {
+    
+    Map<String, Map<Integer, Integer>> soretedGenres = new HashMap<>();
+    Map<String, Integer> allPlays = new HashMap<>();
+    
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Map<Integer, Integer>> sortedGenres = new HashMap<>();
-        
-        
-        for (String genre : genres) {
-            sortedGenres.put(genre, new HashMap<>());
-        }
-        
-        
-        for (int i = 0; i < genres.length; i++) {
-            sortedGenres.get(genres[i]).put(i, plays[i]);
-        }
-        
-        
-        Map<String, Integer> allPlays = new HashMap<>();
-        
-        for (String genre : sortedGenres.keySet()) {
-            allPlays.put(genre, sortedGenres.get(genre).values().stream().mapToInt(n -> n).sum());
-        }
+        record(genres, plays);
+        sumPlays();
         
         List<String> keySetList = new ArrayList<>(allPlays.keySet());
         keySetList.sort((o1, o2) -> allPlays.get(o2) - allPlays.get(o1));
         
-        
-        for (String s : keySetList) {
-            Map<Integer, Integer> maps = sortedGenres.get(s);
+        List<Integer> answer = new ArrayList<>();
+        for (String genre : keySetList) {
+            Map<Integer, Integer> maps = soretedGenres.get(genre);
             List<Integer> idxList = new ArrayList<>(maps.keySet());
-            
             idxList.sort((o1, o2) -> maps.get(o2) - maps.get(o1));
             
             if (idxList.size() <= 1) {
@@ -40,6 +26,15 @@ class Solution {
             }
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();         
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    private void record(String[] genres, int[] plays) {
+        for (String genre : genres) soretedGenres.put(genre, new HashMap<>());
+        for (int i = 0; i < genres.length; i++) soretedGenres.get(genres[i]).put(i, plays[i]);
+    }
+    
+    private void sumPlays() {
+        for (String genre : soretedGenres.keySet()) allPlays.put(genre, soretedGenres.get(genre).values().stream().mapToInt(n -> n).sum());
     }
 }
