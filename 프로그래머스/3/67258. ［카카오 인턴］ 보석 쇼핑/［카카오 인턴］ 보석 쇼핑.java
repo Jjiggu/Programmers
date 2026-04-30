1656dac57e14ddbc8463dc44534e9ae3cadd7fb5
Jihun_Eddie <100821696+Jjiggu@users.noreply.github.com>
2026-04-30 15:29:30 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 81.88 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,27 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int gemTypes = new HashSet<>(Arrays.asList(gems)).size();
-        int left = 0;
-        int length = Integer.MAX_VALUE;
-        int answerLeft = 0, answerRight = 0;
-        Map<String, Integer> gemList = new HashMap<>();
+        
+        int maxGems = new HashSet(Arrays.asList(gems)).size();  // 보석 종류 카운팅 
+        int left = 0;  // 왼쪽 포인터 
+        int minLen = Integer.MAX_VALUE; 
+        int minLeft = 0, minRight = 0;
+        
+        Map<String, Integer> gemList = new HashMap<>();  // 보석 개수 관리를 위한 Map
+        
         
         for (int i = 0; i < gems.length; i++) {
-            gemList.put(gems[i], gemList.getOrDefault(gems[i], 0) + 1);
+            String gemType = gems[i];
             
-            while (gemList.get(gems[left]) > 1) {
+            gemList.put(gemType, gemList.getOrDefault(gemType, 0) + 1);
+            
+            while(gemList.get(gems[left]) > 1) {
                 gemList.put(gems[left], gemList.get(gems[left]) - 1);
                 left++;
             }
             
-            if (gemList.size() == gemTypes && (i - left) < length) {
-                length = i - left;
-                answerLeft = left + 1;
-                answerRight = i + 1;
+            if (gemList.size() == maxGems && (i - left) < minLen) { 
+                minLen = i - left;
+                minLeft = left + 1;
+                minRight = i + 1;
             }
         }
         
-        return new int[]{answerLeft, answerRight};  
+        return new int[]{minLeft, minRight};
     }
 }
